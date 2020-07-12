5d49637a937b2ce3a29af3f2df6cbd0110c955bc
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-07-12 12:43:12 -0700

    feat: redesigning top-n component

--- projects/observability/src/shared/components/top-n/top-n-chart.component.ts
+++ projects/observability/src/shared/components/top-n/top-n-chart.component.ts
@@ -1,18 +1,31 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { TypedSimpleChanges } from '@hypertrace/common';
-import { sortBy } from 'lodash';
-import { HistogramBarData } from '../histogram/histogram-chart.component';
+import { maxBy } from 'lodash';
 
 @Component({
   selector: 'ht-top-n-chart',
+  styleUrls: ['./top-n-chart.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <ht-histogram-chart
-      [data]="this.histogramData"
-      [labelClickable]="this.labelClickable"
-      (labelClick)="this.onLabelClick($event)"
-    >
-    </ht-histogram-chart>
+    <div class="top-n-chart">
+      <ng-container *ngFor="let item of this.itemOptions">
+        <div class="border-top"></div>
+        <div
+          class="label"
+          [traceTooltip]="item.label"
+          (click)="this.onLabelClick(item.label)"
+          [ngClass]="{ clickable: this.labelClickable }"
+        >
+          {{ item.label }}
+        </div>
+        <div class="progress">
+          <div class="progress-value" [ngStyle]="{ width: item.width }"></div>
+        </div>
+        <div class="value">
+          <span>{{ item.value | traceDisplayNumber }} ({{ item.width }})</span>
+        </div>
+      </ng-container>
+    </div>
   `
 })
 export class TopNChartComponent implements OnChanges {
@@ -25,17 +38,45 @@ export class TopNChartComponent implements OnChanges {
   @Output()
   public readonly labelClick: EventEmitter<string> = new EventEmitter();
 
-  public histogramData: HistogramBarData[] = [];
+  public itemOptions: TopNItemOptions[] = [];
 
   public ngOnChanges(changes: TypedSimpleChanges<this>): void {
     if (changes.data && this.data) {
-      this.histogramData = sortBy(this.data, option => -option.value);
+      this.buildItemOptions();
     }
   }
 
   public onLabelClick(label: string): void {
     this.labelClickable && this.labelClick.emit(label);
   }
+
+  private buildItemOptions(): void {
+    this.itemOptions = [];
+    if (this.data.length === 0) {
+      return;
+    }
+
+    let maxValue = maxBy(this.data, option => option.value)?.value;
+    if (maxValue === undefined || maxValue === 0) {
+      maxValue = 1;
+    }
+
+    this.itemOptions = this.data
+      .sort((first, second) => second.value - first.value)
+      .map(
+        (option): TopNItemOptions => ({
+          label: option.label,
+          width: `${((option.value / maxValue!) * 100).toFixed(2)}%`,
+          value: option.value
+        })
+      );
+  }
+}
+export interface TopNData {
+  label: string;
+  value: number;
 }
 
-export type TopNData = HistogramBarData;
+interface TopNItemOptions extends TopNData {
+  width: string;
+}
