345d5d5c8b8a60ae70321716abf63d732472a0ee
SJ <shubhamjindal18@gmail.com>
2020-07-22 21:30:55 +0530

    feat: added gray color palette

--- projects/observability/src/shared/constants/color-palette.ts
+++ projects/observability/src/shared/constants/color-palette.ts
@@ -1,1 +1,2 @@
 export const RED_COLOR_PALETTE = 'red';
+export const GRAY_COLOR_PALETTE = 'gray';
--- src/app/config.module.ts
+++ src/app/config.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ALTERNATE_COLOR_PALETTES, DEFAULT_COLOR_PALETTE, GLOBAL_HEADER_HEIGHT } from '@hypertrace/common';
 import { GRAPHQL_URI } from '@hypertrace/graphql-client';
-import { ENTITY_METADATA, RED_COLOR_PALETTE } from '@hypertrace/observability';
+import { ENTITY_METADATA, GRAY_COLOR_PALETTE, RED_COLOR_PALETTE } from '@hypertrace/observability';
 import { environment } from '../environments/environment';
 import { entityMetadata } from './entity-metadata';
 import { FeatureResolverModule } from './shared/feature-resolver/feature-resolver.module';
@@ -30,6 +30,14 @@ import { FeatureResolverModule } from './shared/feature-resolver/feature-resolve
       useValue: {
         key: RED_COLOR_PALETTE,
         colors: ['#EEC200', '#F7902D', '#E8654B', '#C44660', '#923768', '#5B2F60', '#27244A', '#001429']
+      }
+    },
+    {
+      provide: ALTERNATE_COLOR_PALETTES,
+      multi: true,
+      useValue: {
+        key: GRAY_COLOR_PALETTE,
+        colors: ['#3F474A', '#565F62', '#6C767A', '#828D91', '#97A3A7', '#ACB9BD', '#C1CFD4', '#D6E5EA']
       }
     },
     {
