45aa1c15939419db90d01cc65d041624beac567b
Aaron Steinfeld <aaron@traceable.ai>
2020-07-24 13:26:00 -0400

    test: use dashboard testing tools

--- src/app/home/home.component.test.ts
+++ src/app/home/home.component.test.ts
@@ -1,67 +1,20 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Router } from '@angular/router';
 import { IconLibraryTestingModule } from '@hypertrace/assets-library';
-import {
-  ColorService,
-  NavigationService,
-  RelativeTimeRange,
-  TimeDuration,
-  TimeRangeService,
-  TimeUnit
-} from '@hypertrace/common';
-import { ApplicationAwareDashboardComponent } from '@hypertrace/distributed-tracing';
-import { GraphQlRequestService } from '@hypertrace/graphql-client';
-import { ENTITY_METADATA } from '@hypertrace/observability';
-import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
-import { of } from 'rxjs';
+import { mockDashboardProviders } from '@hypertrace/dashboards/testing';
+import { NavigableDashboardComponent } from '@hypertrace/distributed-tracing';
+import { EntitiesGraphqlQueryBuilderService } from '@hypertrace/observability';
+import { createComponentFactory, mockProvider } from '@ngneat/spectator/jest';
 import { HomeComponent } from './home.component';
 import { HomeModule } from './home.module';
 
 describe('Home Dashboard', () => {
-  let spectator: Spectator<HomeComponent>;
-  let spyLog: jest.SpyInstance;
-  let spyDebug: jest.SpyInstance;
-  let spyInfo: jest.SpyInstance;
-  let spyWarn: jest.SpyInstance;
-  let spyError: jest.SpyInstance;
-
   const createComponent = createComponentFactory({
-    declareComponent: false,
     component: HomeComponent,
-    imports: [HomeModule, HttpClientTestingModule, IconLibraryTestingModule],
-    providers: [
-      {
-        provide: ENTITY_METADATA,
-        useValue: new Map()
-      },
-      mockProvider(ColorService),
-      mockProvider(GraphQlRequestService),
-      mockProvider(Router),
-      mockProvider(NavigationService),
-      mockProvider(TimeRangeService, {
-        getTimeRangeAndChanges: jest.fn().mockReturnValue(of(new RelativeTimeRange(new TimeDuration(1, TimeUnit.Hour))))
-      })
-    ]
+    declareComponent: false,
+    imports: [HomeModule, IconLibraryTestingModule],
+    providers: [mockProvider(EntitiesGraphqlQueryBuilderService), ...mockDashboardProviders]
   });
 
-  beforeEach(() => {
-    spyLog = jest.spyOn(global.console, 'log');
-    spyDebug = jest.spyOn(global.console, 'debug');
-    spyInfo = jest.spyOn(global.console, 'info');
-    spyWarn = jest.spyOn(global.console, 'warn');
-    spyError = jest.spyOn(global.console, 'error');
-
-    spectator = createComponent();
-  });
-
-  test('should receive dashboard ready callback and not log any messages', () => {
-    expect(spectator.query(ApplicationAwareDashboardComponent)!.dashboard).toBeDefined();
-
-    // If the ModelJson has an error in the 'type' string it logs a message
-    expect(spyLog).not.toHaveBeenCalled();
-    expect(spyDebug).not.toHaveBeenCalled();
-    expect(spyInfo).not.toHaveBeenCalled();
-    expect(spyWarn).not.toHaveBeenCalled();
-    expect(spyError).not.toHaveBeenCalled();
+  test('should default to valid json', () => {
+    expect(createComponent().query(NavigableDashboardComponent)).toExist();
   });
 });
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TraceRoute } from '@hypertrace/common';
+import { NavigableDashboardModule } from '@hypertrace/distributed-tracing';
 import { ObservabilityDashboardModule } from '@hypertrace/observability';
 import { TotalErrorsLabelDataSourceModule } from './data/label/total-errors-label-data-source.module';
 import { ObserveSystemRadarDataSourceModule } from './data/system/observe-system-radar-data-source.module';
 import { HomeComponent } from './home.component';
+import { homeDashboard } from './home.dashboard';
 
 const ROUTE_CONFIG: TraceRoute[] = [
   {
@@ -20,7 +22,8 @@ const ROUTE_CONFIG: TraceRoute[] = [
     CommonModule,
     ObservabilityDashboardModule,
     ObserveSystemRadarDataSourceModule,
-    TotalErrorsLabelDataSourceModule
+    TotalErrorsLabelDataSourceModule,
+    NavigableDashboardModule.withDefaultDashboards(homeDashboard)
   ],
   declarations: [HomeComponent]
 })
