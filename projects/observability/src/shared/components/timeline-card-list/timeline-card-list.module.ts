b375cce43837075e7f67b519c376cf64ce577acd
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-08-27 23:05:51 -0700

    feat: adding grouping to timeline cards

--- projects/observability/src/shared/components/timeline-card-list/timeline-card-list.module.ts
+++ projects/observability/src/shared/components/timeline-card-list/timeline-card-list.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormattingModule } from '@hypertrace/common';
+import { ButtonModule } from '@hypertrace/components';
 import { TimelineCardContainerComponent } from './container/timeline-card-container.component';
 import { TimelineCardListComponent } from './timeline-card-list.component';
 
 @NgModule({
-  imports: [CommonModule, FormattingModule],
+  imports: [CommonModule, FormattingModule, ButtonModule],
   declarations: [TimelineCardListComponent, TimelineCardContainerComponent],
   exports: [TimelineCardListComponent, TimelineCardContainerComponent]
 })
