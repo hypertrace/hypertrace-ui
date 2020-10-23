a4d7bb58c3ebd8ca9c97b759acf29896d87511c1
Jake <45181984+jake-bassett@users.noreply.github.com>
2020-10-23 16:47:25 -0700

    feat: timestamp parser works for aggregations (#287)

--- projects/components/src/table/cells/data-parsers/table-cell-timestamp-parser.ts
+++ projects/components/src/table/cells/data-parsers/table-cell-timestamp-parser.ts
@@ -5,14 +5,22 @@ import { CoreTableCellParserType } from '../types/core-table-cell-parser-type';
 @TableCellParser({
   type: CoreTableCellParserType.Timestamp
 })
-export class TableCellTimestampParser extends TableCellParserBase<CellData, CellData, CellData> {
-  public parseValue(cellData: CellData): CellData {
-    return cellData;
+export class TableCellTimestampParser extends TableCellParserBase<CellData, Value, Value> {
+  public parseValue(cellData: CellData): Value {
+    switch (typeof cellData) {
+      case 'number':
+        return cellData;
+      case 'object':
+        return cellData instanceof Date ? cellData : cellData.value;
+      default:
+        return undefined;
+    }
   }
 
-  public parseFilterValue(cellData: CellData): CellData {
+  public parseFilterValue(cellData: CellData): Value {
     return this.parseValue(cellData);
   }
 }
 
-type CellData = Date | number;
+type CellData = Date | number | { value: Date | number };
+type Value = Date | number | undefined;
