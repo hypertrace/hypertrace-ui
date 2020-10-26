f40db3182e2257370e9059ee94cf9c16f3bc8166
Aaron Steinfeld <aaron@traceable.ai>
2020-10-26 08:45:55 -0400

    docs: typo in public api

--- projects/components/src/public-api.ts
+++ projects/components/src/public-api.ts
@@ -274,7 +274,7 @@ export { MenuDropdownModule } from './menu-dropdown/menu-dropdown.module';
 export * from './beta-tag/beta-tag.component';
 export * from './beta-tag/beta-tag.module';
 
-// Multi-select
+// Modal
 export * from './modal/modal';
 export * from './modal/modal.module';
 export * from './modal/modal.service';
