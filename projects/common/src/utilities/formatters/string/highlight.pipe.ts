eee27bdc707927b842c653ec0b2c2fd87db04fcf
Arjunlal B <arjun@traceable.ai>
2020-10-27 13:14:31 +0530

    feat: make highlight pipe customizable with highlight type

--- projects/common/src/utilities/formatters/string/highlight.pipe.ts
+++ projects/common/src/utilities/formatters/string/highlight.pipe.ts
@@ -1,8 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { assertUnreachable } from '../../lang/lang-utils';
 
 @Pipe({ name: 'htHighlight' })
 export class HighlightPipe implements PipeTransform {
-  public transform(fullText: string, highlightedText: string | undefined): string {
-    return highlightedText === undefined ? fullText : fullText.replace(highlightedText, '<mark>$&</mark>');
+  public transform(
+    fullText: string,
+    highlightedText: string | undefined,
+    highlightType: HighlightType = 'mark'
+  ): string {
+    const htmlTag = getHtmlTagForHighlightType(highlightType);
+
+    return highlightedText === undefined ? fullText : fullText.replace(highlightedText, `<${htmlTag}>$&</${htmlTag}>`);
   }
 }
+
+type HighlightType = 'mark' | 'bold' | 'italic';
+
+const getHtmlTagForHighlightType = (type: HighlightType): string | undefined => {
+  switch (type) {
+    case 'bold':
+      return 'b';
+    case 'mark':
+      return 'mark';
+    case 'italic':
+      return 'i';
+    default:
+      assertUnreachable(type);
+  }
+};
