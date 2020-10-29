6ee5694353b95adb8f905f0656e9731a0f6ee504
Jake <45181984+jake-bassett@users.noreply.github.com>
2020-10-29 11:39:34 -0700

    feat: icon cell renderer accepts mapped args (#307)
    * feat: icon cell renderer accepts mapped args
    
    * fix: linting
    
    * fix: prettier styling
    
    * Update projects/components/src/table/cells/data-renderers/icon/icon-table-cell-renderer.component.ts
    
    Co-authored-by: Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
    
    * fix: add constructor to icon renderer
    
    * fix: color
    
    * fix: prettier
    
    Co-authored-by: Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>

--- projects/components/src/table/cells/data-renderers/icon/icon-table-cell-renderer.component.ts
+++ projects/components/src/table/cells/data-renderers/icon/icon-table-cell-renderer.component.ts
@@ -1,5 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { IconSize } from '../../../../icon/icon-size';
+import { TableColumnConfig, TableRow } from '../../../table-api';
+import { IconData } from '../../data-parsers/table-cell-icon-parser';
+import {
+  TABLE_CELL_DATA,
+  TABLE_COLUMN_CONFIG,
+  TABLE_COLUMN_INDEX,
+  TABLE_DATA_PARSER,
+  TABLE_ROW_DATA
+} from '../../table-cell-injection';
+import { TableCellParserBase } from '../../table-cell-parser-base';
 import { TableCellRenderer } from '../../table-cell-renderer';
 import { TableCellRendererBase } from '../../table-cell-renderer-base';
 import { CoreTableCellParserType } from '../../types/core-table-cell-parser-type';
@@ -11,8 +21,13 @@ import { TableCellAlignmentType } from '../../types/table-cell-alignment-type';
   styleUrls: ['./icon-table-cell-renderer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="icon-cell" [ngClass]="{ clickable: this.clickable }">
-      <ht-icon [icon]="this.value" size="${IconSize.Small}" [showTooltip]="true"></ht-icon>
+    <div class="icon-cell" [ngClass]="{ clickable: this.clickable }" *ngIf="this.value">
+      <ht-icon
+        [icon]="this.value.icon"
+        [size]="this.iconSize"
+        [showTooltip]="true"
+        [style.color]="this.value.color"
+      ></ht-icon>
     </div>
   `
 })
@@ -21,4 +36,25 @@ import { TableCellAlignmentType } from '../../types/table-cell-alignment-type';
   alignment: TableCellAlignmentType.Center,
   parser: CoreTableCellParserType.Icon
 })
-export class IconTableCellRendererComponent extends TableCellRendererBase<string> {}
+export class IconTableCellRendererComponent extends TableCellRendererBase<CellData, Value> implements OnInit {
+  public iconSize: IconSize = IconSize.Small;
+
+  public constructor(
+    @Inject(TABLE_COLUMN_CONFIG) columnConfig: TableColumnConfig,
+    @Inject(TABLE_COLUMN_INDEX) index: number,
+    @Inject(TABLE_DATA_PARSER) parser: TableCellParserBase<CellData, Value, unknown>,
+    @Inject(TABLE_CELL_DATA) cellData: CellData,
+    @Inject(TABLE_ROW_DATA) rowData: TableRow
+  ) {
+    super(columnConfig, index, parser, cellData, rowData);
+  }
+
+  public ngOnInit(): void {
+    super.ngOnInit();
+
+    this.iconSize = this.value?.size ?? IconSize.Small;
+  }
+}
+
+type CellData = string | IconData | undefined;
+type Value = IconData | undefined;
