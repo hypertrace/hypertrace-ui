292f4c1e31bc83c99daec6d59cd160f8b70cd14c
Arjunlal B <arjun@traceable.ai>
2020-11-02 22:44:24 +0530

    fix: test for notification component

---
+++ projects/components/src/notification/notification.component.test.ts
@@ -0,0 +1,91 @@
+import { fakeAsync } from '@angular/core/testing';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { IconLibraryTestingModule, IconType } from '@hypertrace/assets-library';
+import { IconComponent, NotificationModule } from '@hypertrace/components';
+import { mockDashboardProviders } from '@hypertrace/dashboards/testing';
+import { createHostFactory, SpectatorHost } from '@ngneat/spectator';
+import { Subject } from 'rxjs';
+import { NotificationComponent, NotificationMode } from './notification.component';
+
+describe('NotificationComponent', () => {
+  let spectator: SpectatorHost<NotificationComponent>;
+
+  const createHost = createHostFactory({
+    component: NotificationComponent,
+    declareComponent: false,
+    imports: [IconLibraryTestingModule, NotificationModule],
+    providers: [...mockDashboardProviders]
+  });
+
+  test('should create success notification correctly', fakeAsync(() => {
+    const closedObserver: Subject<void> = new Subject();
+    const nextSpy = spyOn(closedObserver, 'next');
+    spectator = createHost('<ht-notification></ht-notification>', {
+      providers: [
+        {
+          provide: MAT_SNACK_BAR_DATA,
+          useValue: {
+            mode: NotificationMode.Success,
+            message: 'Success!',
+            closedObserver: closedObserver
+          }
+        }
+      ]
+    });
+
+    expect(spectator.query(IconComponent)?.icon).toEqual(IconType.Checkmark);
+    expect(spectator.query('.text')).toContainText('Success!');
+    spectator.click('.dismiss-icon');
+    spectator.tick();
+
+    expect(nextSpy).toHaveBeenCalled();
+  }));
+
+  test('should create failure notification correctly', fakeAsync(() => {
+    const closedObserver: Subject<void> = new Subject();
+    const nextSpy = spyOn(closedObserver, 'next');
+    spectator = createHost('<ht-notification></ht-notification>', {
+      providers: [
+        {
+          provide: MAT_SNACK_BAR_DATA,
+          useValue: {
+            mode: NotificationMode.Failure,
+            message: 'Failure!',
+            closedObserver: closedObserver
+          }
+        }
+      ]
+    });
+
+    expect(spectator.query(IconComponent)?.icon).toEqual(IconType.Close);
+    expect(spectator.query('.text')).toContainText('Failure!');
+    spectator.click('.dismiss-icon');
+    spectator.tick();
+
+    expect(nextSpy).toHaveBeenCalled();
+  }));
+
+  test('should create info notification correctly', fakeAsync(() => {
+    const closedObserver: Subject<void> = new Subject();
+    const nextSpy = spyOn(closedObserver, 'next');
+    spectator = createHost('<ht-notification></ht-notification>', {
+      providers: [
+        {
+          provide: MAT_SNACK_BAR_DATA,
+          useValue: {
+            mode: NotificationMode.Info,
+            message: 'Information',
+            closedObserver: closedObserver
+          }
+        }
+      ]
+    });
+
+    expect(spectator.query(IconComponent)?.icon).toEqual(IconType.Info);
+    expect(spectator.query('.text')).toContainText('Information');
+    spectator.click('.dismiss-icon');
+    spectator.tick();
+
+    expect(nextSpy).toHaveBeenCalled();
+  }));
+});
