0afbcbd814d0f9afdf349b3b371fba919cb9d55c
Arjunlal B <63222211+arjunlalb@users.noreply.github.com>
2020-11-02 23:48:08 +0530

    feat: notification service (#313)
    * feat: notification service

---
+++ projects/components/src/notification/notification.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig, MatSnackBarRef } from '@angular/material/snack-bar';
+import { Subject } from 'rxjs';
+import { NotificationComponent, NotificationMode } from './notification.component';
+import { NotificationModule } from './notification.module';
+
+@Injectable({ providedIn: NotificationModule })
+export class NotificationService {
+  private readonly snackBarDefaultConfig: MatSnackBarConfig = {
+    horizontalPosition: 'left',
+    verticalPosition: 'bottom',
+    duration: 5000 // Auto dismiss duration. 5 seconds in ms
+  };
+
+  private snackbarRef?: MatSnackBarRef<unknown>;
+  private readonly closedObserver$: Subject<void> = new Subject();
+
+  public constructor(public readonly snackbar: MatSnackBar) {
+    this.closedObserver$.subscribe(() => {
+      this.snackbarRef?.dismiss();
+    });
+  }
+
+  public createSuccessToast(message: string): void {
+    this.snackbarRef = this.snackbar.openFromComponent(NotificationComponent, {
+      ...this.snackBarDefaultConfig,
+      data: {
+        message: message,
+        mode: NotificationMode.Success,
+        closedObserver: this.closedObserver$
+      }
+    });
+  }
+
+  public createFailureToast(message: string): void {
+    this.snackbarRef = this.snackbar.openFromComponent(NotificationComponent, {
+      ...this.snackBarDefaultConfig,
+      duration: 0, // Keep the notification open indefinitely in case of error
+      data: { message: message, mode: NotificationMode.Failure, closedObserver: this.closedObserver$ }
+    });
+  }
+
+  public createInfoToast(message: string): void {
+    this.snackbarRef = this.snackbar.openFromComponent(NotificationComponent, {
+      ...this.snackBarDefaultConfig,
+      data: { message: message, mode: NotificationMode.Info, closedObserver: this.closedObserver$ }
+    });
+  }
+}
