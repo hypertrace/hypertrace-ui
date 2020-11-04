44b53186c0d32692e6a955b7f46aae054fad79c1
Jake Bassett <jake@traceable.ai>
2020-11-04 10:07:09 -0800

    rename: service api dashboard

--- projects/observability/src/pages/apis/service-detail/apis/service-apis-list.module.ts
+++ projects/observability/src/pages/apis/service-detail/apis/service-apis-list.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FilterBarModule } from '@hypertrace/components';
 import { NavigableDashboardModule } from '@hypertrace/distributed-tracing';
 import { ObservabilityDashboardModule } from '../../../../shared/dashboard/observability-dashboard.module';
-import { serviceApisListDashboard } from './service-apis-list-dashboard';
+import { serviceApisListDashboard } from './service-apis-list.dashboard';
 import { ServiceApisListComponent } from './service-apis-list.component';
 
 @NgModule({
