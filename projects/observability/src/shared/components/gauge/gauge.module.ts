b5af7dfbb139b3bd17beabb8a53e5471b42ef6fc
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-11-11 11:24:17 -0800

    refactor: fixing formatting

--- projects/observability/src/shared/components/gauge/gauge.module.ts
+++ projects/observability/src/shared/components/gauge/gauge.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormattingModule } from '@hypertrace/common';
-import { LayoutChangeModule,TooltipModule } from '@hypertrace/components';
+import { LayoutChangeModule, TooltipModule } from '@hypertrace/components';
 import { GaugeComponent } from './gauge.component';
 
 @NgModule({
