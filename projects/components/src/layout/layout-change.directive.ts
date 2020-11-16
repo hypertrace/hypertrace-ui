0709a629705f870ea4573d2a373c04e5c2f6f1f8
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2020-11-16 11:16:42 -0800

    refactor: initializing on init

--- projects/components/src/layout/layout-change.directive.ts
+++ projects/components/src/layout/layout-change.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, EventEmitter, Output } from '@angular/core';
+import { Directive, EventEmitter, OnInit, Output } from '@angular/core';
 import { LayoutChangeService, SubscriptionLifecycle } from '@hypertrace/common';
 
 @Directive({
   selector: '[htLayoutChange]',
   providers: [SubscriptionLifecycle, LayoutChangeService]
 })
-export class LayoutChangeDirective {
+export class LayoutChangeDirective implements OnInit {
   @Output('htLayoutChange')
   public readonly changeEmitter: EventEmitter<void> = new EventEmitter();
 
   public constructor(private readonly layoutChange: LayoutChangeService, subscriptionLifecycle: SubscriptionLifecycle) {
     subscriptionLifecycle.add(layoutChange.layout$.subscribe(this.changeEmitter));
+  }
+
+  public ngOnInit(): void {
     this.layoutChange.initialize();
   }
 }
