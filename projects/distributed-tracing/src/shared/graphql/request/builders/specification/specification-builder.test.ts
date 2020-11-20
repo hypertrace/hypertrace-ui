e5d4c8e06c9a0d0f6ce692163d9a9f8d43af42df
Anand Tiwary <52081890+anandtiwary@users.noreply.github.com>
2020-11-20 14:25:39 -0800

    feat: adding a field specification model (#368)

--- projects/distributed-tracing/src/shared/graphql/request/builders/specification/specification-builder.test.ts
+++ projects/distributed-tracing/src/shared/graphql/request/builders/specification/specification-builder.test.ts
@@ -9,4 +9,18 @@ describe('Specification Builder', () => {
       })
     ).toBeUndefined();
   });
+
+  test('field specs should build correct selection and response', () => {
+    const fieldSpec = specBuilder.fieldSpecificationForKey('test');
+
+    expect(fieldSpec.resultAlias()).toEqual('test');
+    expect(fieldSpec.name).toEqual('test');
+
+    expect(fieldSpec.asGraphQlSelections()).toEqual({ path: 'test' });
+    expect(fieldSpec.extractFromServerData({ test: 'null' })).toBeUndefined();
+
+    expect(fieldSpec.extractFromServerData({ test: 'test value' })).toEqual('test value');
+
+    expect(fieldSpec.asGraphQlOrderByFragment()).toEqual({ key: 'test' });
+  });
 });
