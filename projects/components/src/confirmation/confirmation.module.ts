8b33200040b700c53aaee61fe7fafab93da22b51
Arjunlal B <arjun@traceable.ai>
2020-12-11 12:54:11 +0530

    fix: fix lint error in module

--- projects/components/src/confirmation/confirmation.module.ts
+++ projects/components/src/confirmation/confirmation.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormattingModule } from '@hypertrace/common';
 import { ButtonModule } from '../button/button.module';
 import { ConfirmationModalComponent } from './confirmation-modal.component';
-import { FormattingModule } from '@hypertrace/common';
 
 @NgModule({
   declarations: [ConfirmationModalComponent],
