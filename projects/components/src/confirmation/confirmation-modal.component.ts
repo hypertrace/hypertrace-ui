8b20d21ff0cd8d947fafbcdeac49ce9f5ee0b0cb
Arjunlal B <63222211+arjunlalb@users.noreply.github.com>
2020-12-14 20:07:04 +0530

    feat: support for confirmation description template (#425)
    * feat: support for templates in confirmation modal

--- projects/components/src/confirmation/confirmation-modal.component.ts
+++ projects/components/src/confirmation/confirmation-modal.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, TemplateRef } from '@angular/core';
+import { isString } from 'lodash-es';
 import { ButtonRole } from '../button/button';
 import { ModalRef, MODAL_DATA } from '../modal/modal';
 
@@ -9,7 +10,9 @@ import { ModalRef, MODAL_DATA } from '../modal/modal';
   template: `
     <div class="confirmation-modal">
       <div class="description">
-        {{ this.descriptionText }}
+        <div *ngIf="this.isContentString; else this.content">
+          {{ this.descriptionText }}
+        </div>
       </div>
       <div class="controls">
         <ht-button [label]="this.cancelButtonLabel" role="${ButtonRole.Tertiary}" (click)="this.onCancel()"></ht-button>
@@ -29,13 +32,20 @@ export class ConfirmationModalComponent {
   public readonly confirmButtonLabel: string;
   public readonly cancelButtonLabel: string;
   public readonly confirmButtonRole: ButtonRole;
-  public readonly descriptionText: string;
+  public readonly descriptionText!: string;
+  public readonly content!: TemplateRef<unknown>;
+  public readonly isContentString: boolean;
 
   public constructor(private readonly modalRef: ModalRef<boolean>, @Inject(MODAL_DATA) config: ConfirmationModalData) {
     this.confirmButtonLabel = config.confirmButtonLabel ?? ConfirmationModalComponent.DEFAULT_CONFIRM_LABEL;
     this.confirmButtonRole = config.confirmButtonRole ?? ConfirmationModalComponent.DEFAULT_CONFIRM_ROLE;
     this.cancelButtonLabel = config.cancelButtonLabel ?? ConfirmationModalComponent.DEFAULT_CANCEL_LABEL;
-    this.descriptionText = config.descriptionText;
+    this.isContentString = isString(config.content);
+    if (isString(config.content)) {
+      this.descriptionText = config.content;
+    } else {
+      this.content = config.content;
+    }
   }
 
   public onConfirmation(): void {
@@ -51,5 +61,5 @@ export interface ConfirmationModalData {
   cancelButtonLabel?: string;
   confirmButtonLabel?: string;
   confirmButtonRole?: ButtonRole;
-  descriptionText: string;
+  content: string | TemplateRef<unknown>;
 }
