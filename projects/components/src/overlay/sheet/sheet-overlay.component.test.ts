030457e02b4a32664814c47c6a63f2c8807b5288
Aaron Steinfeld <aaron@traceable.ai>
2021-01-12 15:15:19 -0500

    fix: fix sheet injection to include layout

--- projects/components/src/overlay/sheet/sheet-overlay.component.test.ts
+++ projects/components/src/overlay/sheet/sheet-overlay.component.test.ts
@@ -1,11 +1,11 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GLOBAL_HEADER_HEIGHT, NavigationService } from '@hypertrace/common';
+import { ChangeDetectionStrategy, Component, Inject, Injector, Optional } from '@angular/core';
+import { GLOBAL_HEADER_HEIGHT, LayoutChangeService } from '@hypertrace/common';
 import { POPOVER_DATA } from '@hypertrace/components';
 import { createHostFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
-import { ButtonModule } from '../../button/button.module';
+import { MockComponent } from 'ng-mocks';
+import { ButtonComponent } from '../../button/button.component';
 import { PopoverRef } from '../../popover/popover-ref';
-import { SheetSize } from './sheet';
+import { SheetOverlayConfig, SheetSize, SHEET_DATA } from './sheet';
 import { SheetOverlayComponent } from './sheet-overlay.component';
 
 @Component({
@@ -14,132 +14,103 @@ import { SheetOverlayComponent } from './sheet-overlay.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<div class="test-modal-content">Test Component Content</div> `
 })
-class TestComponent {}
+class TestComponent {
+  public constructor(
+    public readonly injector: Injector,
+    @Optional() @Inject(SHEET_DATA) public readonly data: unknown
+  ) {}
+}
 
 describe('Sheet Overlay component', () => {
   let spectator: Spectator<SheetOverlayComponent>;
 
   const createHost = createHostFactory({
     component: SheetOverlayComponent,
-    imports: [ButtonModule, HttpClientTestingModule],
+    declarations: [MockComponent(ButtonComponent), TestComponent],
+    shallow: true,
+    template: `
+  <ht-sheet-overlay>
+  </ht-sheet-overlay>
+    `,
     providers: [
-      mockProvider(NavigationService),
-      {
-        provide: PopoverRef,
-        useValue: {
-          height: jest.fn(),
-          close: jest.fn()
-        }
-      },
       {
         provide: GLOBAL_HEADER_HEIGHT,
         useValue: 20
-      }
+      },
+      mockProvider(LayoutChangeService)
     ]
   });
 
-  test('should display the title', () => {
-    spectator = createHost(
-      `
-    <ht-sheet-overlay>
-    </ht-sheet-overlay>
-    `,
-      {
-        providers: [
-          {
-            provide: POPOVER_DATA,
-            useValue: {
-              config: {
-                showHeader: true,
-                title: 'test title',
-                content: TestComponent,
-                size: SheetSize.Small
-              }
-            }
+  const createConfiguredHost = (configOverrides: Partial<SheetOverlayConfig> = {}) =>
+    createHost(undefined, {
+      providers: [
+        {
+          provide: PopoverRef,
+          useValue: {
+            height: jest.fn(),
+            close: jest.fn()
           }
-        ]
-      }
-    );
+        },
+        {
+          provide: POPOVER_DATA,
+          deps: [Injector],
+          useFactory: (injector: Injector) => ({
+            config: {
+              showHeader: true,
+              title: 'test title',
+              content: TestComponent,
+              size: SheetSize.Small,
+              ...configOverrides
+            },
+            injector: Injector.create({
+              providers: [
+                {
+                  provide: SHEET_DATA,
+                  useValue: configOverrides.data
+                }
+              ],
+              // Normally, this would be a root injector when this is invoked from a service
+              parent: injector
+            })
+          })
+        }
+      ]
+    });
 
-    expect(spectator.query('.header')).toHaveText('test title');
+  test('should display the title', () => {
+    spectator = createConfiguredHost({
+      title: 'expected title'
+    });
+
+    expect(spectator.query('.header')).toHaveText('expected title');
   });
 
   test('uses the requested size', () => {
-    spectator = createHost(
-      `
-    <ht-sheet-overlay>
-    </ht-sheet-overlay>
-    `,
-      {
-        providers: [
-          {
-            provide: POPOVER_DATA,
-            useValue: {
-              config: {
-                showHeader: true,
-                title: 'test title',
-                content: TestComponent,
-                size: SheetSize.Large
-              }
-            }
-          }
-        ]
-      }
-    );
-
+    spectator = createConfiguredHost({
+      size: SheetSize.Large
+    });
     expect(spectator.query('.sheet-overlay')).toHaveClass('sheet-size-large');
   });
 
   test('closes on close button click', () => {
-    spectator = createHost(
-      `
-    <ht-sheet-overlay>
-    </ht-sheet-overlay>
-    `,
-      {
-        providers: [
-          {
-            provide: POPOVER_DATA,
-            useValue: {
-              config: {
-                showHeader: true,
-                title: 'test title',
-                content: TestComponent,
-                size: SheetSize.Small
-              }
-            }
-          }
-        ]
-      }
-    );
+    spectator = createConfiguredHost();
+
     spectator.click('.close-button');
     expect(spectator.query('.sheet-overlay')).toBeNull();
     expect(spectator.inject(PopoverRef).close).toHaveBeenCalled();
   });
 
   test('displays contents provided', () => {
-    spectator = createHost(
-      `
-      <ht-sheet-overlay>
-      </ht-sheet-overlay>
-      `,
-      {
-        providers: [
-          {
-            provide: POPOVER_DATA,
-            useValue: {
-              config: {
-                showHeader: true,
-                title: 'test title',
-                content: TestComponent,
-                size: SheetSize.Small
-              }
-            }
-          }
-        ]
-      }
-    );
+    spectator = createConfiguredHost();
 
     expect(spectator.query('.content')).toHaveText('Test Component Content');
+  });
+
+  test('exposes correct data to content', () => {
+    spectator = createConfiguredHost({
+      data: 42
+    });
+
+    expect(spectator.query(TestComponent)?.data).toBe(42);
   });
 });
--- projects/distributed-tracing/src/shared/dashboard/interaction/detail-sheet/detail-sheet-interaction-handler.service.test.ts
+++ projects/distributed-tracing/src/shared/dashboard/interaction/detail-sheet/detail-sheet-interaction-handler.service.test.ts
@@ -1,9 +1,6 @@
 import { OverlayService, SheetSize } from '@hypertrace/components';
 import { createServiceFactory, mockProvider, SpectatorService } from '@ngneat/spectator/jest';
-import {
-  DetailSheetInteractionContainerComponent,
-  DETAIL_SHEET_INTERACTION_MODEL
-} from './container/detail-sheet-interaction-container.component';
+import { DetailSheetInteractionContainerComponent } from './container/detail-sheet-interaction-container.component';
 import { DetailSheetInteractionHandlerService } from './detail-sheet-interaction-handler.service';
 
 describe('Overlay service', () => {
@@ -25,21 +22,13 @@ describe('Overlay service', () => {
   test('should call overlay service', () => {
     const detailModel = {};
     spectator.service.showSheet(detailModel, SheetSize.Medium, 'Test Title');
-    const tokenMap = new Map();
-    tokenMap.set(DETAIL_SHEET_INTERACTION_MODEL, detailModel);
 
-    expect(spectator.inject(OverlayService).createSheet).toHaveBeenCalledWith(
-      {
-        content: DetailSheetInteractionContainerComponent,
-        showHeader: true,
-        size: SheetSize.Medium,
-        title: 'Test Title'
-      },
-      expect.objectContaining({
-        parent: expect.objectContaining({
-          records: tokenMap
-        })
-      })
-    );
+    expect(spectator.inject(OverlayService).createSheet).toHaveBeenCalledWith({
+      content: DetailSheetInteractionContainerComponent,
+      showHeader: true,
+      size: SheetSize.Medium,
+      title: 'Test Title',
+      data: detailModel
+    });
   });
 });
