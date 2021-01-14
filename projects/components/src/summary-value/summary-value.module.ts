840d3a38af502f5255283d894b808102867457e4
Arjunlal B <arjun@traceable.ai>
2021-01-14 14:42:57 +0530

    fix: fix lint issue

--- projects/components/src/summary-value/summary-value.module.ts
+++ projects/components/src/summary-value/summary-value.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconModule } from '../icon/icon.module';
+import { LinkModule } from '../link/link.module';
 import { TooltipModule } from '../tooltip/tooltip.module';
 import { SummaryValueComponent } from './summary-value.component';
-import { LinkModule } from '../link/link.module';
 
 @NgModule({
   imports: [CommonModule, IconModule, TooltipModule, LinkModule],
