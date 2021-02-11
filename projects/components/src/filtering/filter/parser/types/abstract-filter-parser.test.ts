ddd7b035e2fcb8dac5aa5413e3c25b1e6450607a
Jake Bassett <jake@traceable.ai>
2021-02-10 16:32:49 -0800

    style: prettier

--- projects/components/src/filtering/filter/parser/types/abstract-filter-parser.test.ts
+++ projects/components/src/filtering/filter/parser/types/abstract-filter-parser.test.ts
@@ -4,20 +4,20 @@ import { ParsedFilter } from '../parsed-filter';
 import { ContainsFilterParser } from './contains-filter-parser';
 
 describe('Filter Parser', () => {
-
   test('correctly parses CONTAINS_KEY with dot', () => {
     const attribute: FilterAttribute = getTestFilterAttribute(FilterAttributeType.StringMap);
     const filterString: string = 'String Map Attribute CONTAINS_KEY http.url';
 
     const containsFilterParser: ContainsFilterParser = new ContainsFilterParser();
-    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(attribute, filterString);
+    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(
+      attribute,
+      filterString
+    );
 
     expect(parsedFilter).toEqual({
       field: 'stringMapAttribute',
       operator: 'CONTAINS_KEY',
-      value: [
-        'http.url'
-      ]
+      value: ['http.url']
     });
   });
 
@@ -26,64 +26,67 @@ describe('Filter Parser', () => {
     const filterString: string = 'String Map Attribute CONTAINS_KEY http:url';
 
     const containsFilterParser: ContainsFilterParser = new ContainsFilterParser();
-    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(attribute, filterString);
+    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(
+      attribute,
+      filterString
+    );
 
     expect(parsedFilter).toEqual({
       field: 'stringMapAttribute',
       operator: 'CONTAINS_KEY',
-      value: [
-        'http:url'
-      ]
+      value: ['http:url']
     });
   });
 
   test('correctly parses CONTAINS_KEY_VALUE for tag with dot and URL', () => {
     const attribute: FilterAttribute = getTestFilterAttribute(FilterAttributeType.StringMap);
-    const filterString: string = 'String Map Attribute.http.url CONTAINS_KEY_VALUE http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24'
+    const filterString: string =
+      'String Map Attribute.http.url CONTAINS_KEY_VALUE http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24';
 
     const containsFilterParser: ContainsFilterParser = new ContainsFilterParser();
-    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(attribute, filterString);
+    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(
+      attribute,
+      filterString
+    );
 
     expect(parsedFilter).toEqual({
       field: 'stringMapAttribute',
       operator: 'CONTAINS_KEY_VALUE',
-      value: [
-        'http.url',
-        'http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24'
-      ]
+      value: ['http.url', 'http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24']
     });
   });
 
   test('correctly parses CONTAINS_KEY_VALUE for tag with colon and URL', () => {
     const attribute: FilterAttribute = getTestFilterAttribute(FilterAttributeType.StringMap);
-    const filterString: string = 'String Map Attribute.http:url CONTAINS_KEY_VALUE http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24'
+    const filterString: string =
+      'String Map Attribute.http:url CONTAINS_KEY_VALUE http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24';
 
     const containsFilterParser: ContainsFilterParser = new ContainsFilterParser();
-    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(attribute, filterString);
+    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(
+      attribute,
+      filterString
+    );
 
     expect(parsedFilter).toEqual({
       field: 'stringMapAttribute',
       operator: 'CONTAINS_KEY_VALUE',
-      value: [
-        'http:url',
-        'http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24'
-      ]
+      value: ['http:url', 'http://dataservice:9394/userreview?productId=f2620500-8b55-4fab-b7a2-fe8af6f5ae24']
     });
   });
 
   test('correctly parses CONTAINS_KEY_VALUE for tag with colon and empty string', () => {
     const attribute: FilterAttribute = getTestFilterAttribute(FilterAttributeType.StringMap);
-    const filterString: string = 'String Map Attribute.http:url CONTAINS_KEY_VALUE ""'
+    const filterString: string = 'String Map Attribute.http:url CONTAINS_KEY_VALUE ""';
     const containsFilterParser: ContainsFilterParser = new ContainsFilterParser();
-    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(attribute, filterString);
+    const parsedFilter: ParsedFilter<unknown> | undefined = containsFilterParser.parseFilterString(
+      attribute,
+      filterString
+    );
 
     expect(parsedFilter).toEqual({
       field: 'stringMapAttribute',
       operator: 'CONTAINS_KEY_VALUE',
-      value: [
-        'http:url',
-        '""'
-      ]
+      value: ['http:url', '""']
     });
   });
 });
