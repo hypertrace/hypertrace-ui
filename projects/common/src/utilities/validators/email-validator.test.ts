e8dae5b628c8838475aec876b490c24858275295
Arjunlal B <arjun@traceable.ai>
2021-03-04 21:21:50 +0530

    fix: update test strings

--- projects/common/src/utilities/validators/email-validator.test.ts
+++ projects/common/src/utilities/validators/email-validator.test.ts
@@ -2,19 +2,19 @@ import { areEmailAddressesValid, isEmailAddressValid } from '@hypertrace/common'
 
 describe('Email validator', () => {
   test('can validate email correctly', () => {
-    expect(isEmailAddressValid('test@traceable.ai')).toBe(true);
-    expect(isEmailAddressValid('test.email@traceable.ai')).toBe(true);
-    expect(isEmailAddressValid('test+983tonq@traceable.ai')).toBe(true);
-    expect(isEmailAddressValid('test@traceable.')).toBe(false);
+    expect(isEmailAddressValid('test@test.ai')).toBe(true);
+    expect(isEmailAddressValid('test.email@test.ai')).toBe(true);
+    expect(isEmailAddressValid('test+983tonq@test.ai')).toBe(true);
+    expect(isEmailAddressValid('test@test.')).toBe(false);
     expect(isEmailAddressValid('test')).toBe(false);
-    expect(isEmailAddressValid('test@traceable-')).toBe(false);
-    expect(isEmailAddressValid('tes\t@traceable-')).toBe(false);
-    expect(isEmailAddressValid('test@@traceable.ai')).toBe(false);
-    expect(isEmailAddressValid('test@@traceable.ai')).toBe(false);
+    expect(isEmailAddressValid('test@test-')).toBe(false);
+    expect(isEmailAddressValid('tes\t@test-')).toBe(false);
+    expect(isEmailAddressValid('test@@test.ai')).toBe(false);
+    expect(isEmailAddressValid('test@@test.ai')).toBe(false);
   });
 
   test('can validate email lists correctly', () => {
-    expect(areEmailAddressesValid(['test@traceable.ai', 'test.email@traceable.ai'])).toBe(true);
-    expect(areEmailAddressesValid(['test@traceable.ai', 'test.email@@traceable.ai'])).toBe(false);
+    expect(areEmailAddressesValid(['test@test.ai', 'test.email@test.ai'])).toBe(true);
+    expect(areEmailAddressesValid(['test@test.ai', 'test.email@@test.ai'])).toBe(false);
   });
 });
