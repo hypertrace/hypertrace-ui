80569f2b2ae9540fca127e551b6ca0398d4b1b3f
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-03-18 12:05:47 -0700

    refactor: reverting code

--- projects/observability/src/shared/dashboard/data/graphql/table/explore/explore-table-data-source.model.ts
+++ projects/observability/src/shared/dashboard/data/graphql/table/explore/explore-table-data-source.model.ts
@@ -80,7 +80,6 @@ export class ExploreTableDataSourceModel extends TableDataSourceModel {
       interval: this.interval?.getDuration(),
       limit: request.position.limit * 2, // Prefetch 2 pages
       offset: request.position.startIndex,
-      includeTotal: true,
       orderBy: request.sort && [
         {
           direction: request.sort.direction,
