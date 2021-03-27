6f2fc3b516df3f7356f4affe3b71ad9e4e9bc541
Arjunlal B <arjun@traceable.ai>
2021-03-27 19:14:52 +0530

    fix: fix test

--- projects/distributed-tracing/src/shared/components/span-detail/exit-calls/span-exit-calls.component.test.ts
+++ projects/distributed-tracing/src/shared/components/span-detail/exit-calls/span-exit-calls.component.test.ts
@@ -1,9 +1,11 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { fakeAsync } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { IconLibraryTestingModule } from '@hypertrace/assets-library';
 import { NavigationService } from '@hypertrace/common';
 import { runFakeRxjs } from '@hypertrace/test-utils';
 import { createHostFactory, mockProvider, Spectator } from '@ngneat/spectator/jest';
+import { EMPTY } from 'rxjs';
 import { SpanExitCallsComponent } from './span-exit-calls.component';
 import { SpanExitCallsModule } from './span-exit-calls.module';
 
@@ -14,7 +16,14 @@ describe('SpanExitCallsComponent', () => {
     component: SpanExitCallsComponent,
     imports: [SpanExitCallsModule, HttpClientTestingModule, IconLibraryTestingModule],
     declareComponent: false,
-    providers: [mockProvider(NavigationService)]
+    providers: [
+      mockProvider(ActivatedRoute, {
+        queryParamMap: EMPTY
+      }),
+      mockProvider(NavigationService, {
+        navigation$: EMPTY
+      })
+    ]
   });
 
   it('should render data correctly', fakeAsync(() => {
@@ -30,7 +39,7 @@ describe('SpanExitCallsComponent', () => {
             { name: 'name 1', calls: '10' },
             { name: 'name 2', calls: '11' }
           ],
-          totalCount: 3
+          totalCount: 2
         }
       });
     });
