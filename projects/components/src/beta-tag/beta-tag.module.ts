a5510fcba065b9120170c6e61905316ceee87cf8
Arjunlal B <arjun@traceable.ai>
2021-04-06 10:15:12 +0530

    feat: support displaying beta tag in left nav item

--- projects/components/src/beta-tag/beta-tag.module.ts
+++ projects/components/src/beta-tag/beta-tag.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LabelTagModule } from '../label-tag/label-tag.module';
+import { TooltipModule } from '../tooltip/tooltip.module';
 import { BetaTagComponent } from './beta-tag.component';
 
 @NgModule({
   declarations: [BetaTagComponent],
-  imports: [LabelTagModule],
+  imports: [LabelTagModule, TooltipModule],
   exports: [BetaTagComponent]
 })
 export class BetaTagModule {}
