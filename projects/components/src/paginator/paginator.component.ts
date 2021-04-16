77bd6b5cee7d339b25b1003184908bb6eab745ca
Patricio Albizu <albizupatricio@github.com>
2021-04-16 10:55:54 -0300

    fix: solving nit

--- projects/components/src/paginator/paginator.component.ts
+++ projects/components/src/paginator/paginator.component.ts
@@ -19,7 +19,7 @@ import { PaginationProvider } from './paginator-api';
   styleUrls: ['./paginator.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="paginator" *ngIf="this.totalItems && this.totalItems > pageSizeOptions[0]">
+    <div class="paginator" *ngIf="this.totalItems && this.totalItems > this.pageSize">
       <ht-label
         class="label"
         label="{{ this.firstItemNumberForPage() }}-{{ this.lastItemNumberForPage() }} of {{ this.totalItems }}"
