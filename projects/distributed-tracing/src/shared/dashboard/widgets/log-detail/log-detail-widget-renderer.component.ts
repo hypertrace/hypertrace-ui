10f3d37e30a6bf8d1572f72a580d7cea77e884dc
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2021-05-06 15:40:17 +0530

    fix: test case

--- projects/distributed-tracing/src/shared/dashboard/widgets/log-detail/log-detail-widget-renderer.component.ts
+++ projects/distributed-tracing/src/shared/dashboard/widgets/log-detail/log-detail-widget-renderer.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject } from '@angular/core';
 import { Dictionary } from '@hypertrace/common';
 import { ListViewHeader, ListViewRecord } from '@hypertrace/components';
 import { WidgetRenderer } from '@hypertrace/dashboards';
 import { Renderer } from '@hypertrace/hyperdash';
+import { RendererApi, RENDERER_API } from '@hypertrace/hyperdash-angular';
 import { isEmpty } from 'lodash-es';
 import { Observable } from 'rxjs';
 import { LogDetailWidgetModel } from './log-detail-widget.model';
@@ -23,6 +24,12 @@ import { LogDetailWidgetModel } from './log-detail-widget.model';
   `
 })
 export class LogDetailWidgetRendererComponent extends WidgetRenderer<LogDetailWidgetModel, Dictionary<unknown>> {
+  public constructor(
+    @Inject(RENDERER_API) public readonly api: RendererApi<LogDetailWidgetModel>,
+    protected readonly changeDetector: ChangeDetectorRef
+  ) {
+    super(api, changeDetector);
+  }
   public readonly header: ListViewHeader = { keyLabel: 'key', valueLabel: 'value' };
 
   protected fetchData(): Observable<Dictionary<unknown>> {
