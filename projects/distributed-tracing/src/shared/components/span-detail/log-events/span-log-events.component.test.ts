4d1af9a038d6d50992fa5fa88f844ab09a0e7ea9
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2021-05-06 18:36:15 +0530

    fix: test case

--- projects/distributed-tracing/src/shared/components/span-detail/log-events/span-log-events.component.test.ts
+++ projects/distributed-tracing/src/shared/components/span-detail/log-events/span-log-events.component.test.ts
@@ -1,8 +1,10 @@
 import { IconLibraryTestingModule } from '@hypertrace/assets-library';
+import { DashboardModule } from '@hypertrace/dashboards';
 import { mockDashboardProviders } from '@hypertrace/dashboards/testing';
 import { NavigableDashboardComponent } from '@hypertrace/distributed-tracing';
 import { EntitiesGraphqlQueryBuilderService } from '@hypertrace/observability';
 import { createComponentFactory, mockProvider } from '@ngneat/spectator/jest';
+import { GraphQlDataSourceModule } from '../../../dashboard/data/graphql/graphql-data-source.module';
 import { SpanLogEventsComponent } from './span-log-events.component';
 import { SpanLogEventsModule } from './span-log-events.module';
 
@@ -10,12 +12,14 @@ describe('SpanLogEventsComponent', () => {
   const createComponent = createComponentFactory({
     component: SpanLogEventsComponent,
     declareComponent: false,
-    imports: [SpanLogEventsModule, IconLibraryTestingModule],
+    imports: [SpanLogEventsModule, GraphQlDataSourceModule, DashboardModule, IconLibraryTestingModule],
     providers: [mockProvider(EntitiesGraphqlQueryBuilderService), ...mockDashboardProviders]
   });
 
   test('should create dashboard', () => {
     const spectator = createComponent();
     expect(spectator.query(NavigableDashboardComponent)).toExist();
+    expect(spectator.query(NavigableDashboardComponent)!.navLocation).toBe('SPAN_LOG_EVENTS');
+    expect(spectator.query(NavigableDashboardComponent)!.padding).toBe('0');
   });
 });
