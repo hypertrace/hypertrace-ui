a49ff7439ad491b536f1b692172f057c8d3870e1
Jake Bassett <jake@traceable.ai>
2021-05-12 12:25:46 -0700

    fix: simplify

--- projects/common/src/utilities/formatters/enum/display-string-enum.ts
+++ projects/common/src/utilities/formatters/enum/display-string-enum.ts
@@ -1,15 +1,9 @@
-import { isNil, startCase } from 'lodash-es';
+import { capitalize, isNil, lowerCase } from 'lodash-es';
 
 export const displayStringEnum = (provided?: string, defaultValue: string = '-', separator: string = ' '): string => {
   if (isNil(provided)) {
     return defaultValue;
   }
 
-  // This removes dashes and underscores and gives all words initial caps
-  const startCased = startCase(provided);
-
-  const startCasedSeparated = startCased.replace(' ', separator);
-
-  // We only want the first word capitalized.
-  return `${startCasedSeparated[0]}${startCasedSeparated.substr(1).toLowerCase()}`;
+  return capitalize(lowerCase(provided)).replace(' ', separator);
 };
