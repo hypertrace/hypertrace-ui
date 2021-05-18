5294178b4d6a7e6989295e8261244209b5aec89a
Jake Bassett <jake@traceable.ai>
2021-05-18 11:10:37 -0700

    feat: memoize record method call so it is only called once

--- projects/distributed-tracing/src/shared/components/log-events/log-events-table.module.ts
+++ projects/distributed-tracing/src/shared/components/log-events/log-events-table.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormattingModule } from '@hypertrace/common';
+import { FormattingModule, MemoizeModule } from '@hypertrace/common';
 import { IconModule, ListViewModule, TableModule, TooltipModule } from '@hypertrace/components';
 import { LogEventsTableComponent } from './log-events-table.component';
 @NgModule({
-  imports: [CommonModule, TableModule, IconModule, TooltipModule, FormattingModule, ListViewModule],
+  imports: [CommonModule, TableModule, IconModule, TooltipModule, FormattingModule, ListViewModule, MemoizeModule],
   declarations: [LogEventsTableComponent],
   exports: [LogEventsTableComponent]
 })
