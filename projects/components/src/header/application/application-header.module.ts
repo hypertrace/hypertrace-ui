0a8b903b5098074d8f382c396943637f11ceafea
Arjunlal B <63222211+arjunlalb@users.noreply.github.com>
2021-05-21 09:57:39 +0530

    refactor: remove unused components (#868)

--- projects/components/src/header/application/application-header.module.ts
+++ projects/components/src/header/application/application-header.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { IconModule } from '../../icon/icon.module';
 import { SelectModule } from '../../select/select.module';
 import { TimeRangeModule } from '../../time-range/time-range.module';
-import { SpaceSelectorComponent } from '../space-selector/space-selector.component';
 import { ApplicationHeaderComponent } from './application-header.component';
 
 @NgModule({
   imports: [IconModule, SelectModule, TimeRangeModule],
-  declarations: [ApplicationHeaderComponent, SpaceSelectorComponent],
+  declarations: [ApplicationHeaderComponent],
   exports: [ApplicationHeaderComponent]
 })
 export class ApplicationHeaderModule {}
