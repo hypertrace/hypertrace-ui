ccfd2af62a2a6cd3171614ba6ba7d289b9b1c5fa
Patricio Albizu <albizupatricio@github.com>
2021-05-24 15:33:38 -0300

    feat: adding comment to omit line

--- projects/components/src/table/cells/data-parsers/table-cell-no-op-parser.ts
+++ projects/components/src/table/cells/data-parsers/table-cell-no-op-parser.ts
@@ -11,7 +11,7 @@ export class TableCellNoOpParser<T = unknown> extends TableCellParserBase<T, T, 
   }
 
   public parseFilterValue(cellData: T | { filterValue?: string }): string | undefined {
-    // tslint:disable-next-line: no-unused-expression
+    // tslint:disable-next-line:strict-type-predicates
     if (typeof cellData === 'object' && cellData !== null && 'filterValue' in cellData) {
       return cellData.filterValue;
     }
