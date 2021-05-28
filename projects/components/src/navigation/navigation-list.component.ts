586bab82c59a258911ad5a75570f7e99ee156dfd
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-05-27 20:59:56 -0700

    refactor: fixing lint

--- projects/components/src/navigation/navigation-list.component.ts
+++ projects/components/src/navigation/navigation-list.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IconType } from '@hypertrace/assets-library';
-import { NavigationParamsType, NavigationService } from '@hypertrace/common';
+import { NavigationService } from '@hypertrace/common';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { IconSize } from '../icon/icon-size';
