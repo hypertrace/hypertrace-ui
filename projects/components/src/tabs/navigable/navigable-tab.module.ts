49a5db7c6ce25545253a83847aebe94752560f89
Arjunlal B <arjun@traceable.ai>
2021-05-29 10:57:07 +0530

    fix: use ht-link in tab component

--- projects/components/src/tabs/navigable/navigable-tab.module.ts
+++ projects/components/src/tabs/navigable/navigable-tab.module.ts
@@ -4,12 +4,13 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
 import { FeatureConfigCheckModule } from '../../feature-check/feature-config-check.module';
 import { LetAsyncModule } from '../../let-async/let-async.module';
+import { LinkModule } from '../../link/link.module';
 import { NavigableTabGroupComponent } from './navigable-tab-group.component';
 import { NavigableTabComponent } from './navigable-tab.component';
 
 @NgModule({
   declarations: [NavigableTabGroupComponent, NavigableTabComponent],
   exports: [NavigableTabGroupComponent, NavigableTabComponent],
-  imports: [MatTabsModule, CommonModule, RouterModule, LetAsyncModule, FeatureConfigCheckModule]
+  imports: [MatTabsModule, CommonModule, RouterModule, LetAsyncModule, FeatureConfigCheckModule, LinkModule]
 })
 export class NavigableTabModule {}
