b6e3a6c39a96e7f08253b04e396187c44fad5cbd
Arjunlal B <arjun@traceable.ai>
2021-06-04 12:12:06 +0530

    fix: add comment

--- projects/observability/src/shared/components/cartesian/chart.ts
+++ projects/observability/src/shared/components/cartesian/chart.ts
@@ -23,6 +23,7 @@ export interface Series<TInterval> {
   units?: string;
   summary?: Summary;
   color: string;
+  // Override the default color string using a method that takes data point as input
   getColor?(datum?: TInterval): string;
   name: string;
   symbol?: SeriesSymbol;
