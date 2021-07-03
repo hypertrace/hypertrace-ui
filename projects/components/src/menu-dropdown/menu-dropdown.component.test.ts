bf55e3e2e53d50d0679c6e4c8560bbd2b8691cbd
Arjunlal B <arjun@traceable.ai>
2021-07-03 15:38:06 +0530

    fix: fix lint issue

--- projects/components/src/menu-dropdown/menu-dropdown.component.test.ts
+++ projects/components/src/menu-dropdown/menu-dropdown.component.test.ts
@@ -80,9 +80,7 @@ describe('Menu dropdown Component', () => {
   });
 
   test('should not show the dropdown if no items to show.', () => {
-    spectator = createHost(
-      `<ht-menu-dropdown label="Settings" icon="${IconType.MoreHorizontal}"></ht-menu-dropdown>`,
-    );
+    spectator = createHost(`<ht-menu-dropdown label="Settings" icon="${IconType.MoreHorizontal}"></ht-menu-dropdown>`);
 
     expect(spectator.query('ht-popover-trigger')).not.toExist();
   });
