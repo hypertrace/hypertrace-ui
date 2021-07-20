0e1c4ecac19430065b9e03a594f20311b3aa35ee
Patricio Albizu <patricioalbizu@Patricios-MacBook-Pro.local>
2021-07-20 16:50:44 -0300

    feat: Adding unlimited value

--- projects/observability/src/shared/components/bar-gauge/bar-gauge.component.ts
+++ projects/observability/src/shared/components/bar-gauge/bar-gauge.component.ts
@@ -27,8 +27,12 @@ import {
       <div class="header-data" [ngClass]="this.display">
         <div *ngIf="this.title" class="title">{{ this.title | htDisplayTitle }}</div>
         <div class="count">
-          {{ this.totalValue | number }} / {{ this.maxValue | number }}
-          <span class="units" *ngIf="this.units">{{ this.units }}</span>
+          <span>{{ this.totalValue | number }}</span>
+          <span class="units" *ngIf="this.units && this.isUnlimited"> {{ this.units }}</span>
+          <span> / </span> 
+          <span *ngIf="!this.isUnlimited">{{ this.maxValue | number }}</span>
+          <span class="unlimited-symbol" *ngIf="this.isUnlimited">&#8734;</span>
+          <span class="units" *ngIf="this.units && !this.isUnlimited"> {{ this.units }}</span>
         </div>
       </div>
       <div class="bar">
@@ -83,6 +87,9 @@ export class BarGaugeComponent implements OnChanges, AfterViewInit {
 
   @Input()
   public display: BarGaugeStyle = BarGaugeStyle.Regular;
+
+  @Input()
+  public isUnlimited: boolean = false;
 
   public barSegments: BarSegment[] = [];
   public totalValue: number = 0;
