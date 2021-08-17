4ee6013d3a4331b9880e785ebc7e5c4d148b5a44
palbizu <79482271+palbizu@users.noreply.github.com>
2021-08-17 19:19:01 -0300

    feat: adding icon properties (#1063)
    * feat: adding icon properties
    
    * feat: adding border settings
    
    * feat: adding border settings
    
    * feat: fixing inputs
    
    Co-authored-by: Patricio Albizu <patricioalbizu@Patricios-MacBook-Pro.local>

--- projects/components/src/select/select.component.ts
+++ projects/components/src/select/select.component.ts
@@ -61,6 +61,9 @@ import { SelectSize } from './select-size';
                 [icon]="this.getPrefixIcon(selected)"
                 [size]="this.iconSize"
                 [color]="selected?.iconColor"
+                [borderType]="selected?.iconBorderType"
+                [borderColor]="selected?.iconBorderColor"
+                [borderRadius]="selected?.iconBorderRadius"
               >
               </ht-icon>
               <ht-label class="trigger-label" [label]="selected?.selectedLabel || selected?.label || this.placeholder">
@@ -123,6 +126,9 @@ import { SelectSize } from './select-size';
                   [icon]="item.icon"
                   size="${IconSize.Small}"
                   [color]="item.iconColor"
+                  [borderType]="item?.iconBorderType"
+                  [borderColor]="item?.iconBorderColor"
+                  [borderRadius]="item?.iconBorderRadius"
                 >
                 </ht-icon>
                 <span class="label">{{ item.label }}</span>
