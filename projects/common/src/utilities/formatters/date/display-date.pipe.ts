3fb28ce9b0ff183989d1b7fee39fb4a6a0d3edad
Aaron Steinfeld <aaron@traceable.ai>
2021-08-19 19:30:28 -0400

    fix: support anything coercer supports for date parsing

--- projects/common/src/utilities/formatters/date/display-date.pipe.ts
+++ projects/common/src/utilities/formatters/date/display-date.pipe.ts
@@ -1,11 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { isNil } from 'lodash-es';
+import { DateCoercer } from '../../coercers/date-coercer';
 import { DateFormatOptions, DateFormatter } from './date-formatter';
 
 @Pipe({
   name: 'htDisplayDate'
 })
 export class DisplayDatePipe implements PipeTransform {
-  public transform(value?: Date | number | null, options: DateFormatOptions = {}): string {
-    return value !== null ? new DateFormatter(options).format(value) : '-';
+  private readonly dateCoercer: DateCoercer = new DateCoercer();
+  public transform(value?: string | Date | number | null, options: DateFormatOptions = {}): string {
+    return isNil(value) ? '-' : new DateFormatter(options).format(this.dateCoercer.coerce(value));
   }
 }
