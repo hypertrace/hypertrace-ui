87ece1d80eefa91dd89c26a7059f3d4722c5816b
Sandeep Sharma <78212931+itssharmasandeep@users.noreply.github.com>
2021-08-20 21:45:01 +0530

    chore: upgrade to angular 12 (#1073)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,8 +55,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
-import 'zone.js/dist/zone-patch-rxjs';
+import 'zone.js'; // Included with Angular CLI.
+import 'zone.js/plugins/zone-patch-rxjs';
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
