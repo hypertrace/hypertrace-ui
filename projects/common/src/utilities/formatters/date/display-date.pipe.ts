7f7f28053feac890c4354ed539cc261332ff85a9
Aaron Steinfeld <aaron@traceable.ai>
2021-08-19 20:07:54 -0400

    refactor: coercer in formatter

--- projects/common/src/utilities/formatters/date/display-date.pipe.ts
+++ projects/common/src/utilities/formatters/date/display-date.pipe.ts
@@ -1,16 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { isNil } from 'lodash-es';
-import { DateCoercer } from '../../coercers/date-coercer';
 import { DateFormatOptions, DateFormatter } from './date-formatter';
 
 @Pipe({
   name: 'htDisplayDate'
 })
 export class DisplayDatePipe implements PipeTransform {
-  private readonly dateCoercer: DateCoercer = new DateCoercer();
   public transform(value?: string | Date | number | null, options: DateFormatOptions = {}): string {
-    const coercedDate = this.dateCoercer.coerce(value);
-
-    return isNil(coercedDate) ? '-' : new DateFormatter(options).format(coercedDate);
+    return isNil(value) ? '-' : new DateFormatter(options).format(value);
   }
 }
--- projects/components/src/table/cells/data-parsers/table-cell-timestamp-parser.test.ts
+++ projects/components/src/table/cells/data-parsers/table-cell-timestamp-parser.test.ts
@@ -7,14 +7,17 @@ describe('table cell timestamp parser', () => {
   const dateMillis = date.getTime();
 
   test('can parse date strings', () => {
+    // tslint:disable-next-line: no-object-literal-type-assertion
     expect(new TableCellTimestampParser({} as Injector).parseValue(dateString)).toEqual(date);
   });
 
   test('can parse numbers', () => {
+    // tslint:disable-next-line: no-object-literal-type-assertion
     expect(new TableCellTimestampParser({} as Injector).parseValue(dateMillis)).toEqual(dateMillis);
   });
 
   test('can parse date objects', () => {
+    // tslint:disable-next-line: no-object-literal-type-assertion
     expect(new TableCellTimestampParser({} as Injector).parseValue(date)).toEqual(date);
   });
 });
