3e681a65ef3ed494864b38c3f63b2cb1aecf028a
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2021-09-08 11:42:55 +0530

    feat: template functionality for radio button label

--- projects/components/src/radio/radio-option.ts
+++ projects/components/src/radio/radio-option.ts
@@ -1,5 +1,7 @@
+import { TemplateRef } from '@angular/core';
+
 export interface RadioOption {
   value: string;
-  label: string;
+  label: string | TemplateRef<unknown>;
   description?: string;
 }
