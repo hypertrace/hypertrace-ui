125cb266dc0c35ca5f4818b4e5ffe71b5ea7e4ea
Jyothish Jose <jyothish.jose@razorpay.com>
2021-09-21 12:02:32 +0530

    fix: added test cases for span and trace search

---
+++ projects/observability/src/pages/trace-detail/trace-detail.page.component.test.ts
@@ -0,0 +1,51 @@
+import { fakeAsync, tick } from '@angular/core/testing';
+import { MemoizeModule, NavigationService } from '@hypertrace/common';
+import { LoadAsyncModule } from '@hypertrace/components';
+import { createComponentFactory, mockProvider } from '@ngneat/spectator';
+import { MockComponent } from 'ng-mocks';
+import { of } from 'rxjs';
+import { ExploreFilterLinkComponent } from '../../shared/components/explore-filter-link/explore-filter-link.component';
+import { ExplorerService } from '../explorer/explorer-service';
+import { TraceDetailPageComponent } from './trace-detail.page.component';
+import { TraceDetails, TraceDetailService } from './trace-detail.service';
+describe('Trace Details Page Component', () => {
+  const mockTraceDetails: TraceDetails = {
+    id: 'test-id',
+    entrySpanId: 'test-123',
+    type: 'trace-type',
+    timeString: 'test-time-string',
+    titleString: 'test-title',
+    startTime: 'test-start-time'
+  };
+
+  const createComponent = createComponentFactory({
+    component: TraceDetailPageComponent,
+    shallow: true,
+    providers: [
+      mockProvider(NavigationService),
+      mockProvider(ExplorerService, {
+        buildNavParamsWithFilters: jest.fn().mockReturnValue(of('traceId_eq_test-123'))
+      })
+    ],
+    imports: [LoadAsyncModule, MemoizeModule],
+    componentProviders: [
+      mockProvider(TraceDetailService, {
+        fetchTraceDetails: jest.fn().mockReturnValue(of(mockTraceDetails))
+      })
+    ],
+    declarations: [MockComponent(ExploreFilterLinkComponent)]
+  });
+
+  test('should render content correctly', fakeAsync(() => {
+    const spectator = createComponent();
+
+    spectator.click('.label');
+    tick();
+    expect(spectator.inject(NavigationService).navigateBack).toHaveBeenCalled();
+  }));
+
+  test('should render explorer link component', fakeAsync(() => {
+    const spectator = createComponent();
+    expect(spectator.query(ExploreFilterLinkComponent)?.paramsOrUrl).toBe('traceId_eq_test-123');
+  }));
+});
