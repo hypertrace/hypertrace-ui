a8b308df738729215e4fc8602fd3efae68b128d0
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-09-21 17:34:11 -0700

    refactor: updating api for htTrack directive

--- projects/common/src/telemetry/track/track.directive.ts
+++ projects/common/src/telemetry/track/track.directive.ts
@@ -1,17 +1,60 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { Directive, ElementRef, Input, OnChanges, OnDestroy } from '@angular/core';
+import { TypedSimpleChanges } from '../../utilities/types/angular-change-object';
+import { TrackUserEventsType } from '../telemetry';
 import { UserTelemetryImplService } from '../user-telemetry-impl.service';
 
 @Directive({
   selector: '[htTrack]'
 })
-export class TrackDirective {
+export class TrackDirective implements OnChanges, OnDestroy {
   @Input('htTrack')
-  public name!: string;
+  public userEvents: TrackUserEventsType[] = [TrackUserEventsType.Click];
 
-  public constructor(private readonly userTelemetryImplService: UserTelemetryImplService) {}
+  @Input('htTrackLabel')
+  public label?: string;
 
-  @HostListener('click', ['$event'])
-  public trackClick(event: MouseEvent): void {
-    this.userTelemetryImplService.trackEvent(`Click: ${this.name}`, { target: event.target, type: event.type });
+  private readonly subscription: Subscription = new Subscription();
+  private trackedEventLabel: string = '';
+
+  public constructor(
+    private readonly host: ElementRef,
+    private readonly userTelemetryImplService: UserTelemetryImplService
+  ) {}
+
+  public ngOnChanges(changes: TypedSimpleChanges<this>): void {
+    if (changes.userEvents) {
+      this.setupListeners();
+    }
+
+    if (changes.label) {
+      this.trackedEventLabel = this.label ?? (this.host.nativeElement as HTMLElement)?.tagName;
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.clearListeners();
+  }
+
+  private setupListeners(): void {
+    this.clearListeners();
+    this.subscription.add(
+      ...this.userEvents.map(userEvent =>
+        fromEvent<MouseEvent>(this.host.nativeElement, userEvent).subscribe(eventObj =>
+          this.trackUserEvent(userEvent, eventObj)
+        )
+      )
+    );
+  }
+
+  private clearListeners(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private trackUserEvent(userEvent: TrackUserEventsType, eventObj: MouseEvent): void {
+    this.userTelemetryImplService.trackEvent(`${userEvent}: ${this.trackedEventLabel}`, {
+      target: eventObj.target,
+      type: userEvent
+    });
   }
 }
