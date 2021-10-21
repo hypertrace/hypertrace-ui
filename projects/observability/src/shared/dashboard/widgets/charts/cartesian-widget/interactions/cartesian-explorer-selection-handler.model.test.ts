bcff6536bc331cdd69d8f3b0fb3f211e589efb21
Jyothish Jose <jyothish.jose@razorpay.com>
2021-10-21 11:18:09 +0530

    feat: cartesian drilldown - test cases added

--- projects/observability/src/shared/dashboard/widgets/charts/cartesian-widget/interactions/cartesian-explorer-selection-handler.model.test.ts
+++ projects/observability/src/shared/dashboard/widgets/charts/cartesian-widget/interactions/cartesian-explorer-selection-handler.model.test.ts
@@ -1,31 +1,56 @@
 import { NavigationService } from '@hypertrace/common';
 import { createModelFactory } from '@hypertrace/dashboards/testing';
 import { mockProvider } from '@ngneat/spectator/jest';
+import { of } from 'rxjs';
 import { ExplorerService } from '../../../../../../pages/explorer/explorer-service';
 import { CartesianExplorerSelectionHandlerModel } from './cartesian-explorer-selection-handler.model';
 
 describe('Cartesian Explorer Selection Handler Model', () => {
+  const selectedData = [
+    {
+      dataPoint: {
+        timestamp: 'Wed Oct 20 2021 00:25:00 GMT+0530 (India Standard Time)'
+      }
+    },
+    {
+      dataPoint: {
+        timestamp: 'Wed Oct 20 2021 12:25:00 GMT+0530 (India Standard Time)'
+      }
+    }
+  ];
+
+  const navigationUrl = {
+    navType: 'in-app',
+    path: '/explorer',
+    queryParams: {
+      filter: ['startTime_gte_1634669700000', 'endTime_lte_1634712900000'],
+      scope: 'endpoint-traces'
+    }
+  };
+
   const buildModel = createModelFactory({
-    providers: [mockProvider(ExplorerService), mockProvider(NavigationService)]
+    providers: [
+      mockProvider(ExplorerService, {
+        buildNavParamsWithFilters: jest.fn().mockReturnValue(of(navigationUrl))
+      }),
+      mockProvider(NavigationService, {
+        navigate: jest.fn()
+      })
+    ]
   });
 
-  test('calls navigateToExplorer with correct parameters', () => {
+  test('calls navigateToExplorer with correct parameters1', () => {
     const spectator = buildModel(CartesianExplorerSelectionHandlerModel);
     const explorerService = spectator.get(ExplorerService);
     const navService = spectator.get(NavigationService);
 
-    const navigationUrl1 = {
-      navType: 'in-app',
-      path: '/explorer',
-      queryParams: {
-        filter: ['startTime_gte_1634669700000', 'endTime_lte_1634712900000'],
-        scope: 'endpoint-traces'
-      }
-    };
+    spectator.model.execute(selectedData);
+    expect(explorerService.buildNavParamsWithFilters).toHaveBeenCalled();
+    expect(navService.navigate).toHaveBeenCalled();
 
-    spyOn(explorerService, 'buildNavParamsWithFilters').and.returnValue(navigationUrl1);
-    setTimeout(() => {
-      expect(navService.navigate).toHaveBeenCalledWith(navigationUrl1);
-    });
+    // spyOn(explorerService, 'buildNavParamsWithFilters').and.returnValue(navigationUrl);
+    // setTimeout(() => {
+    //   expect(navService.navigate).toHaveBeenCalledWith(navigationUrl);
+    // });
   });
 });
