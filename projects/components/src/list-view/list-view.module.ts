1ce25c8c596a8cdfbdbe9a48e7fc762ca82ff441
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-12-07 15:08:19 -0800

    refactor: fix formatting

--- projects/components/src/list-view/list-view.module.ts
+++ projects/components/src/list-view/list-view.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ListViewValueRendererDirective } from './list-view-value-renderer.directive';
 import { ListViewKeyRendererDirective } from './list-view-key-renderer.directive';
+import { ListViewValueRendererDirective } from './list-view-value-renderer.directive';
 import { ListViewComponent } from './list-view.component';
 
 @NgModule({
