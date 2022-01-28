93126714c4574754a54f0591fd57e4639813dbc6
Christian Quinn <christian@FewGoodTaters.local>
2022-01-28 09:25:25 -0400

    fix: linting

--- projects/components/src/load-async/load-async.module.ts
+++ projects/components/src/load-async/load-async.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconModule } from '../icon/icon.module';
+import { LayoutChangeModule } from '../layout/layout-change.module';
 import { MessageDisplayModule } from '../message-display/message-display.module';
 import { SkeletonModule } from '../skeleton/skeleton.module';
 import { LoadAsyncDirective } from './load-async.directive';
 import { LoaderComponent } from './loader/loader.component';
 import { LoadAsyncWrapperComponent } from './wrapper/load-async-wrapper.component';
-import { LayoutChangeModule } from '../layout/layout-change.module';
 
 @NgModule({
   declarations: [LoadAsyncDirective, LoadAsyncWrapperComponent, LoaderComponent],
