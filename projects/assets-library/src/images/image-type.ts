7ed675e7bfabf2760e964cc215ae44be2b0b3173
Christian Quinn <christian@FewGoodTaters.local>
2022-01-31 17:58:32 -0400

    refactor: requested changes

--- projects/assets-library/src/images/image-type.ts
+++ projects/assets-library/src/images/image-type.ts
@@ -1,11 +1,3 @@
 export const enum ImagesAssetPath {
-  ErrorPage = 'assets/images/error-page.svg',
-  LoaderSpinner = 'assets/images/loader-spinner.gif',
-  LoaderPage = 'assets/images/loader-page.gif',
-  LoaderExpandableRow = 'assets/images/loader-expandable-row.gif',
-  LoaderLogo = 'assets/images/loader-logo.svg',
-  LoaderCartesion = 'assets/images/loader-cartesian.svg',
-  LoaderCartesionColumn = 'assets/images/loader-cartesian-column.svg',
-  LoaderRadar = 'assets/images/loader-radar.svg',
-  LoaderTopology = 'assets/images/loader-topology.svg'
+  ErrorPage = 'assets/images/error-page.svg'
 }
