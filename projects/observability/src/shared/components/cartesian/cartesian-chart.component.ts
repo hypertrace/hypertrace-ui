36ad0226de075d9f782aaa13f3b0b86d23d9bbe9
Sandeep Sharma <78212931+itssharmasandeep@users.noreply.github.com>
2022-02-02 19:21:50 +0530

    Update projects/observability/src/shared/components/cartesian/cartesian-chart.component.ts
    Co-authored-by: Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>

--- projects/observability/src/shared/components/cartesian/cartesian-chart.component.ts
+++ projects/observability/src/shared/components/cartesian/cartesian-chart.component.ts
@@ -105,7 +105,7 @@ export class CartesianChartComponent<TData> implements OnChanges, OnDestroy {
         )
       );
 
-    if (this.isSelectionHandlerAvailable) {
+    if (this. isRangeSelectionEnabled) {
       this.chart.withEventListener(ChartEvent.Select, selectedData => {
         this.selectionChange.emit(selectedData);
       });
