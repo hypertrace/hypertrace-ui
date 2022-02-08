438b7dc729fb4a2e06ca531648659d2f8ce872c4
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2022-02-09 00:21:12 +0530

    test(validator): update test cases

--- projects/common/src/utilities/validators/domain-validator.test.ts
+++ projects/common/src/utilities/validators/domain-validator.test.ts
@@ -1,7 +1,16 @@
-import { areDomainsValid } from '@hypertrace/common';
+import { areDomainsValid } from './domain-validator';
 
-const VALID_DOMAINS = ['google.com', 'fig.io', 'traceable.ai', 'f4ncy-domain.cc'];
-const INVALID_DOMAINS = ['https://google.com', 'google.com/', '123123@.scom', 'g00#.dd', 's.ss', 'www.112323.com'];
+const VALID_DOMAINS = [
+  'google.com',
+  'fig.io',
+  'hypertrace.org',
+  'f4ncy-domain.cc',
+  'hypertrace.co.uk',
+  'HYPERTRACE.ORG',
+  'HYPER.TRACE.ORG',
+  'hyper.trace.org'
+];
+const INVALID_DOMAINS = ['https://google.com', 'google.com/', '123123@.scom', 'g00#.dd', 'www.112323.com'];
 describe('Domain validator', () => {
   test('can validate email lists correctly', () => {
     expect(areDomainsValid(VALID_DOMAINS)).toBeTruthy();
--- projects/common/src/utilities/validators/domain-validator.ts
+++ projects/common/src/utilities/validators/domain-validator.ts
@@ -2,4 +2,4 @@ export const DOMAIN_REGEX = new RegExp(/^.+\.[a-zA-Z]{2,}$/);
 
 export const isDomainValid = (domain: string) => DOMAIN_REGEX.test(domain);
 
-export const areDomainsValid = (domains: string[]) => domains.filter(domain => !isDomainValid(domain)).length === 0;
+export const areDomainsValid = (domains: string[]) => domains.every(isDomainValid);
