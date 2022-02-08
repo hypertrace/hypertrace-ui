949171dbc1a39c2abecaa1d3c8d20d47c60de26a
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2022-02-08 16:47:31 +0530

    chore: fix lint issue

--- projects/common/src/utilities/validators/domain-validator.ts
+++ projects/common/src/utilities/validators/domain-validator.ts
@@ -1,7 +1,5 @@
 export const DOMAIN_REGEX = new RegExp(/^.+\.[a-zA-Z]{2,}$/);
 
-export const isDomainValid = (domain: string) => {
-  return DOMAIN_REGEX.test(domain);
-};
+export const isDomainValid = (domain: string) => DOMAIN_REGEX.test(domain);
 
 export const areDomainsValid = (domains: string[]) => domains.filter(domain => !isDomainValid(domain)).length === 0;
