90e9b281fc21e39191c8100de6b392eba084a8c3
Jyothish Jose <jyothish.jose@razorpay.com>
2022-02-14 01:38:50 +0530

    feat: iframe widget - lint errors fixed

--- projects/observability/src/shared/dashboard/widgets/iframe-widget/iframe-widget-renderer.component.spec.ts
+++ projects/observability/src/shared/dashboard/widgets/iframe-widget/iframe-widget-renderer.component.spec.ts
@@ -1,5 +1,5 @@
 import { mockDashboardWidgetProviders } from '@hypertrace/dashboards/testing';
-import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
 
 import { IframeWidgetRendererComponent } from './iframe-widget-renderer.component';
 
