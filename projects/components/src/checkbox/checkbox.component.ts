1143acb33615354c55c6613d114d1b6db72a1978
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2022-02-15 12:48:05 +0530

    feat(checkbox): add control value accessor implementation

--- projects/components/src/checkbox/checkbox.component.ts
+++ projects/components/src/checkbox/checkbox.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
@@ -8,31 +9,66 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
   template: `
     <mat-checkbox
       labelPosition="after"
-      [checked]="this.checked"
-      [disabled]="this.disabled"
-      (change)="onCheckboxChange($event)"
+      [checked]="this.isChecked"
+      [disabled]="this.isDisabled"
+      (change)="this.onCheckboxChange($event)"
       class="ht-checkbox"
-      [ngClass]="{ disabled: this.disabled }"
+      [ngClass]="{ disabled: this.isDisabled }"
     >
       <ht-label class="label" *ngIf="this.label !== undefined && this.label !== ''" [label]="this.label"></ht-label>
     </mat-checkbox>
-  `
+  `,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: CheckboxComponent,
+      multi: true
+    }
+  ]
 })
-export class CheckboxComponent {
+export class CheckboxComponent implements ControlValueAccessor {
   @Input()
   public label?: string;
 
   @Input()
-  public checked: boolean | undefined;
+  public set checked(checked: boolean | undefined) {
+    this.isChecked = checked ?? false;
+  }
 
   @Input()
-  public disabled: boolean | undefined;
+  public set disabled(disabled: boolean | undefined) {
+    this.isDisabled = disabled ?? false;
+  }
 
   @Output()
   public readonly checkedChange: EventEmitter<boolean> = new EventEmitter();
 
+  public isChecked: boolean = false;
+  public isDisabled: boolean = false;
+
+  private onTouched: () => void = () => {};
+  private onChanged: (value: boolean) => void = () => {};
+
   public onCheckboxChange(event: MatCheckboxChange): void {
-    this.checked = event.checked;
-    this.checkedChange.emit(this.checked);
+    this.isChecked = event.checked;
+    this.checkedChange.emit(this.isChecked);
+    this.onChanged(this.isChecked);
+    this.onTouched();
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChanged = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  public writeValue(isChecked: boolean): void {
+    this.isChecked = isChecked;
   }
 }
