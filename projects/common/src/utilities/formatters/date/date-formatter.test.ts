d2cea356ddc5f7639dfd649e32b990c2f4b52b0e
palbizu <79482271+palbizu@users.noreply.github.com>
2022-02-22 11:56:00 -0300

    feat: Adding date formatter (#1447)
    * feat: Adding date formatter
    
    * feat: adding test
    
    Co-authored-by: Patricio Albizu <patricioalbizu@Patricios-MacBook-Pro.local>

--- projects/common/src/utilities/formatters/date/date-formatter.test.ts
+++ projects/common/src/utilities/formatters/date/date-formatter.test.ts
@@ -1,9 +1,17 @@
-import { DateFormatter } from './date-formatter';
+import { DateFormatMode, DateFormatter } from './date-formatter';
 
 describe('Date formatter', () => {
   const dateString = '2021-08-19T23:35:45.861Z';
 
   test('can format a date string', () => {
     expect(new DateFormatter().format(dateString)).toEqual('19 Aug 2021 11:35 PM');
+  });
+
+  test('can format a date string with month and year only', () => {
+    expect(
+      new DateFormatter({
+        mode: DateFormatMode.MonthAndYearOnly
+      }).format(dateString)
+    ).toEqual('Aug 21');
   });
 });
