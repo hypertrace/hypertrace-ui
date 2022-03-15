32e07e52d67b2eb0462fe105a2054839db26636d
Patricio Albizu <patricioalbizu@Patricios-MacBook-Pro.local>
2022-03-15 18:22:53 -0300

    feat: fixing lint

--- projects/components/src/draggable-list/draggable-list.module.ts
+++ projects/components/src/draggable-list/draggable-list.module.ts
@@ -1,8 +1,8 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { DraggableListComponent } from './draggable-list.component';
 import { DraggableItemComponent } from './draggable-item/draggable-item.component';
+import { DraggableListComponent } from './draggable-list.component';
 
 @NgModule({
   declarations: [DraggableListComponent, DraggableItemComponent],
