5c0cfe6510201690b2481ee4138925740df8e415
Christian <35982795+Christian862@users.noreply.github.com>
2022-03-18 20:29:41 -0300

    refactor: fix FF string

--- projects/common/src/constants/application-constants.ts
+++ projects/common/src/constants/application-constants.ts
@@ -3,5 +3,5 @@ import { InjectionToken } from '@angular/core';
 export const GLOBAL_HEADER_HEIGHT = new InjectionToken<string>('Global Header Height');
 
 export const enum ApplicationFeature {
-  PageTimeRange = 'ui.page-time-range-'
+  PageTimeRange = 'ui.page-time-range'
 }
