d818ada6412bee4c968bde94c56506c199065364
Shreyansh Sahu <shreyansh.sahu@traceable.ai>
2022-03-25 02:41:43 +0530

    fix(duration-pipe): updated duration pipe for long display

--- projects/common/src/utilities/formatters/duration/display-duration.pipe.ts
+++ projects/common/src/utilities/formatters/duration/display-duration.pipe.ts
@@ -1,11 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { durationFormatter } from './duration-formatter';
+import { TimeUnit } from 'projects/common/src/public-api';
+import { TimeDuration, UnitStringType } from 'projects/common/src/time/time-duration';
+
 
 @Pipe({
   name: 'htDisplayDuration'
 })
 export class DisplayDurationPipe implements PipeTransform {
-  public transform(millis?: number): string {
-    return durationFormatter(millis);
+  public transform(millis?: number, unitStringType: UnitStringType = UnitStringType.Short): string {
+    if(millis === undefined)
+    return '-';
+
+    if(unitStringType === UnitStringType.Short)
+    return new TimeDuration(millis!, TimeUnit.Millisecond).toString();
+
+    return new TimeDuration(millis!, TimeUnit.Millisecond).toMultiUnitString();
   }
 }
