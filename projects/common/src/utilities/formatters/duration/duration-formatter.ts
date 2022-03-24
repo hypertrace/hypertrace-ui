15fa767116b89c209e10e2e71bdaeb361871f04d
Shreyansh Sahu <shreyansh.sahu@traceable.ai>
2022-03-25 02:52:42 +0530

    fix(duration-formatter): restored deleted files

---
+++ projects/common/src/utilities/formatters/duration/duration-formatter.ts
@@ -0,0 +1,32 @@
+export const durationFormatter = (duration?: number): string => {
+    const dayInMs: number = 1000 * 60 * 60 * 24;
+    if (duration === undefined) {
+      return '-';
+    }
+    const days = Math.abs(Math.trunc(duration / dayInMs));
+  
+    const date = new Date(duration);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+    const milliseconds = date.getUTCMilliseconds();
+  
+    if (days !== 0) {
+      return hours === 0 && minutes === 0 ? `${days}d` : `${days}d ${hours}h ${doubleDigit(minutes)}m`;
+    }
+  
+    if (hours !== 0) {
+      return `${hours}h ${doubleDigit(minutes)}m`;
+    }
+    if (minutes !== 0) {
+      return `${minutes}m ${doubleDigit(seconds)}s`;
+    }
+    if (seconds !== 0) {
+      return `${seconds}s`;
+    }
+  
+    return `${milliseconds}ms`;
+  };
+  
+  const doubleDigit = (value: number): string => (value < 10 ? `0${value}` : `${value}`);
+  
