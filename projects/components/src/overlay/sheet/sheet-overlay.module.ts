b715f8717a78df936e2f10249d3ce528dc8f8510
Arjunlal B <arjun@traceable.ai>
2022-03-25 13:15:45 +0530

    feat(sheet overlay): support for nav params config

--- projects/components/src/overlay/sheet/sheet-overlay.module.ts
+++ projects/components/src/overlay/sheet/sheet-overlay.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonModule } from '../../button/button.module';
 import { IconModule } from '../../icon/icon.module';
+import { OpenInNewTabModule } from '../../open-in-new-tab/open-in-new-tab.module';
 import { TooltipModule } from '../../tooltip/tooltip.module';
 import { SheetOverlayComponent } from './sheet-overlay.component';
 
 @NgModule({
-  imports: [CommonModule, ButtonModule, TooltipModule, IconModule],
+  imports: [CommonModule, ButtonModule, TooltipModule, IconModule, OpenInNewTabModule],
   declarations: [SheetOverlayComponent],
   exports: [SheetOverlayComponent]
 })
