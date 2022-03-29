052cf430147bb6064839161ad79854159f82c7ec
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2022-03-29 10:47:48 +0530

    feat(span-detail): replace tab with toggle group

--- projects/observability/src/shared/components/span-detail/span-detail.component.ts
+++ projects/observability/src/shared/components/span-detail/span-detail.component.ts
@@ -1,6 +1,17 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  TemplateRef,
+  ViewChild
+} from '@angular/core';
 import { TypedSimpleChanges } from '@hypertrace/common';
+import { ToggleItem } from '@hypertrace/components';
 import { isEmpty } from 'lodash-es';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { SpanData } from './span-data';
 import { SpanDetailLayoutStyle } from './span-detail-layout-style';
 import { SpanDetailTab } from './span-detail-tab';
@@ -23,40 +34,49 @@ import { SpanDetailTab } from './span-detail-tab';
       <div class="summary-container">
         <ng-content></ng-content>
       </div>
-
-      <ht-tab-group
-        class="tabs-group"
-        [activeTabLabel]="this.activeTabLabel"
-        (activeTabLabelChange)="this.onActiveTabLabelChange($event)"
+      <ht-toggle-group
+        class="traffic-type-toggle"
+        [activeItem]="activeTab$ | async"
+        [items]="this.tabs"
+        (activeItemChange)="this.tabChange($event)"
       >
-        <ht-tab label="${SpanDetailTab.Request}" *ngIf="this.showRequestTab">
-          <ht-span-request-detail
-            class="request"
-            [layout]="this.layout"
-            [requestHeaders]="this.spanData.requestHeaders"
-            [requestCookies]="this.spanData.requestCookies"
-            [requestBody]="this.spanData.requestBody"
-          ></ht-span-request-detail>
-        </ht-tab>
-        <ht-tab label="${SpanDetailTab.Response}" *ngIf="this.showResponseTab">
-          <ht-span-response-detail
-            class="response"
-            [layout]="this.layout"
-            [responseHeaders]="this.spanData.responseHeaders"
-            [responseCookies]="this.spanData.responseCookies"
-            [responseBody]="this.spanData.responseBody"
-          ></ht-span-response-detail>
-        </ht-tab>
-        <ht-tab label="${SpanDetailTab.Attributes}" class="attributes">
-          <ht-span-tags-detail [tags]="this.spanData.tags"></ht-span-tags-detail>
-        </ht-tab>
-        <ht-tab label="${SpanDetailTab.ExitCalls}" *ngIf="this.showExitCallsTab">
-          <ht-span-exit-calls [exitCalls]="this.spanData.exitCallsBreakup"></ht-span-exit-calls>
-        </ht-tab>
-        <ht-tab *ngIf="this.showLogEventsTab" label="Logs" [labelTag]="this.totalLogEvents">
-          <ht-log-events-table [logEvents]="this.spanData?.logEvents"></ht-log-events-table>
-        </ht-tab>
-      </ht-tab-group>
+      </ht-toggle-group>
+
+      <div>
+        <ng-container *ngTemplateOutlet="this.template$ | async"></ng-container>
+      </div>
+
+      <ng-template #requestTemplate>
+        <ht-span-request-detail
+          class="request"
+          [layout]="this.layout"
+          [requestHeaders]="this.spanData.requestHeaders"
+          [requestCookies]="this.spanData.requestCookies"
+          [requestBody]="this.spanData.requestBody"
+        ></ht-span-request-detail>
+      </ng-template>
+
+      <ng-template #responseTemplate>
+        <ht-span-response-detail
+          class="response"
+          [layout]="this.layout"
+          [responseHeaders]="this.spanData.responseHeaders"
+          [responseCookies]="this.spanData.responseCookies"
+          [responseBody]="this.spanData.responseBody"
+        ></ht-span-response-detail>
+      </ng-template>
+
+      <ng-template #attributesTemplate>
+        <ht-span-tags-detail [tags]="this.spanData.tags"></ht-span-tags-detail>
+      </ng-template>
+
+      <ng-template #exitCallsTemplate>
+        <ht-span-exit-calls [exitCalls]="this.spanData.exitCallsBreakup"></ht-span-exit-calls>
+      </ng-template>
+
+      <ng-template #logEventsTemplate>
+        <ht-log-events-table [logEvents]="this.spanData?.logEvents"></ht-log-events-table>
+      </ng-template>
     </div>
   `
 })
@@ -79,11 +99,38 @@ export class SpanDetailComponent implements OnChanges {
   @Output()
   public readonly closed: EventEmitter<void> = new EventEmitter<void>();
 
+  @ViewChild('requestTemplate')
+  public requestTemplate?: TemplateRef<unknown>;
+
+  @ViewChild('responseTemplate')
+  public responseTemplate?: TemplateRef<unknown>;
+
+  @ViewChild('attributesTemplate')
+  public attributesTemplate?: TemplateRef<unknown>;
+
+  @ViewChild('exitCallsTemplate')
+  public exitCallsTemplate?: TemplateRef<unknown>;
+
+  @ViewChild('logEventsTemplate')
+  public logEventsTemplate?: TemplateRef<unknown>;
+
   public showRequestTab?: boolean;
   public showResponseTab?: boolean;
   public showExitCallsTab?: boolean;
   public showLogEventsTab?: boolean;
   public totalLogEvents?: number;
+
+  public readonly tabs: ToggleItem<SpanDetailTab>[] = [
+    { label: SpanDetailTab.Request, value: SpanDetailTab.Request },
+    { label: SpanDetailTab.Response, value: SpanDetailTab.Response },
+    { label: SpanDetailTab.Attributes, value: SpanDetailTab.Attributes }
+  ];
+
+  private readonly activeTabSubject = new BehaviorSubject<ToggleItem<SpanDetailTab>>(this.tabs[0]);
+  public readonly activeTab$ = this.activeTabSubject.asObservable();
+
+  private readonly templateSubject = new Subject<TemplateRef<unknown>>();
+  public readonly template$ = this.templateSubject.asObservable();
 
   public ngOnChanges(changes: TypedSimpleChanges<this>): void {
     if (changes.spanData) {
@@ -98,11 +145,47 @@ export class SpanDetailComponent implements OnChanges {
       this.showExitCallsTab = !isEmpty(this.spanData?.exitCallsBreakup);
       this.showLogEventsTab = !isEmpty(this.spanData?.logEvents);
       this.totalLogEvents = (this.spanData?.logEvents ?? []).length;
+      if (this.showExitCallsTab && this.tabs.findIndex(({ value }) => value === SpanDetailTab.ExitCalls) === -1) {
+        this.tabs.push({ label: SpanDetailTab.ExitCalls, value: SpanDetailTab.ExitCalls });
+      }
+      if (this.showLogEventsTab && this.tabs.findIndex(({ value }) => value === SpanDetailTab.Logs) === -1) {
+        this.tabs.push({ label: SpanDetailTab.Logs, value: SpanDetailTab.Logs });
+      }
+    }
+    if (changes.activeTabLabel) {
+      const tab = this.tabs.find(({ value }) => value === this.activeTabLabel);
+      if (tab) {
+        this.activeTabSubject.next(tab);
+      }
     }
   }
 
   public onActiveTabLabelChange(tabLabel: SpanDetailTab): void {
     this.activeTabLabel = tabLabel;
     this.activeTabLabelChange.emit(tabLabel);
+  }
+
+  public tabChange(tab: ToggleItem<SpanDetailTab>): void {
+    if (tab.value) {
+      const template = this.getTemplateForTab(tab.value);
+      if (template) {
+        this.templateSubject.next(template);
+      }
+    }
+  }
+
+  public getTemplateForTab(tab: SpanDetailTab): TemplateRef<unknown> | undefined {
+    switch (tab) {
+      case SpanDetailTab.Request:
+        return this.requestTemplate;
+      case SpanDetailTab.Response:
+        return this.responseTemplate;
+      case SpanDetailTab.Attributes:
+        return this.attributesTemplate;
+      case SpanDetailTab.ExitCalls:
+        return this.exitCallsTemplate;
+      case SpanDetailTab.Logs:
+        return this.logEventsTemplate;
+    }
   }
 }
