aec5d909ecc94f12398953e0aba90d4757f4adb3
Aaron Steinfeld <aaron@traceable.ai>
2022-03-29 22:41:28 -0400

    feat: add storage scoping

--- projects/common/src/utilities/browser/storage/abstract-storage.ts
+++ projects/common/src/utilities/browser/storage/abstract-storage.ts
@@ -1,17 +1,36 @@
 import { Observable, Subject } from 'rxjs';
 import { distinctUntilChanged, filter, map, startWith } from 'rxjs/operators';
+import { DictionaryStorageImpl } from './dictionary-storage-impl';
 
 // A small abstraction on browser's storage for mocking and cleaning up the API a bit
 export abstract class AbstractStorage {
   private readonly changeSubject: Subject<string> = new Subject();
-  public constructor(private readonly storage: Storage) {}
+  private readonly scopedStorage?: DictionaryStorageImpl;
+
+  public constructor(private readonly storage: Storage, private readonly scopeConfig?: ScopedStorageConfiguration) {
+    if (scopeConfig) {
+      this.scopedStorage = DictionaryStorageImpl.fromString(storage.getItem(scopeConfig.scopeKey) ?? '{}');
+    }
+  }
 
   public contains(key: string): boolean {
-    return this.storage.getItem(key) !== null;
+    return this.get(key) !== undefined;
   }
 
   public get<T extends string = string>(key: string): T | undefined {
-    const value = this.storage.getItem(key);
+    const value =
+      this.scopeConfig && !this.scopeConfig.fallbackPolicy
+        ? this.scopedStorage!.getItem(key) // Only read from scoped storage if in use AND no fallback policy
+        : this.scopedStorage?.getItem(key) ?? this.storage.getItem(key);
+
+    if (
+      this.scopeConfig?.fallbackPolicy === 'read-and-migrate' &&
+      value !== null &&
+      this.scopedStorage?.getItem(key) === null
+    ) {
+      this.set(key, value);
+      this.storage.removeItem(key);
+    }
 
     return value !== null ? (value as T) : undefined;
   }
@@ -26,12 +45,25 @@ export abstract class AbstractStorage {
   }
 
   public set(key: string, value: string): void {
-    this.storage.setItem(key, value);
+    (this.scopedStorage ?? this.storage).setItem(key, value);
+    this.flushAnyScopedStorage();
     this.changeSubject.next(key);
   }
 
   public delete(key: string): void {
-    this.storage.removeItem(key);
+    (this.scopedStorage ?? this.storage).removeItem(key);
+    this.flushAnyScopedStorage();
     this.changeSubject.next(key);
   }
+
+  private flushAnyScopedStorage(): void {
+    if (this.scopedStorage && this.scopeConfig?.scopeKey) {
+      this.storage.setItem(this.scopeConfig.scopeKey, this.scopedStorage?.toJsonString());
+    }
+  }
+}
+
+export interface ScopedStorageConfiguration {
+  scopeKey: string;
+  fallbackPolicy?: 'read-only' | 'read-and-migrate';
 }
