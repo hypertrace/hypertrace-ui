6b634d95f4b120edc82f9f7cb78df540e8a96e66
Arjunlal B <arjun@traceable.ai>
2022-04-04 12:59:46 +0530

    fix(styling): styling fixes for cell renderers

--- projects/components/src/table/cells/table-cells.module.ts
+++ projects/components/src/table/cells/table-cells.module.ts
@@ -10,6 +10,7 @@ import { FilterModalModule } from '../../filtering/filter-modal/filter-modal.mod
 import { IconModule } from '../../icon/icon.module';
 import { PopoverModule } from '../../popover/popover.module';
 import { TooltipModule } from '../../tooltip/tooltip.module';
+import { XMoreModule } from '../../x-more/x-more.module';
 import { TableHeaderCellRendererComponent } from '../header/table-header-cell-renderer.component';
 import { TableCellBooleanParser } from './data-parsers/table-cell-boolean-parser';
 import { TableCellIconParser } from './data-parsers/table-cell-icon-parser';
@@ -22,14 +23,24 @@ import { DurationTableCellRendererComponent } from './data-renderers/duration/du
 import { StringEnumTableCellRendererComponent } from './data-renderers/enum/string-enum-table-cell-renderer.component';
 import { IconTableCellRendererComponent } from './data-renderers/icon/icon-table-cell-renderer.component';
 import { NumericTableCellRendererComponent } from './data-renderers/numeric/numeric-table-cell-renderer.component';
-import { RelativeTimestampTableCellRendererComponent } from './data-renderers/relative-timestamp/relative-timestamp-table-cell-renderer.component';
-import { StringArrayTableCellRendererComponent } from './data-renderers/string-array/string-array-table-cell-renderer.component';
+import {
+  RelativeTimestampTableCellRendererComponent
+} from './data-renderers/relative-timestamp/relative-timestamp-table-cell-renderer.component';
+import {
+  StringArrayTableCellRendererComponent
+} from './data-renderers/string-array/string-array-table-cell-renderer.component';
 import { TableDataCellRendererComponent } from './data-renderers/table-data-cell-renderer.component';
-import { TextWithCopyActionTableCellRendererComponent } from './data-renderers/text-with-copy/text-with-copy-table-cell-renderer.component';
+import {
+  TextWithCopyActionTableCellRendererComponent
+} from './data-renderers/text-with-copy/text-with-copy-table-cell-renderer.component';
 import { TextTableCellRendererComponent } from './data-renderers/text/text-table-cell-renderer.component';
 import { TimeAgoTableCellRendererComponent } from './data-renderers/time-ago/time-ago-table-cell-renderer.component';
-import { TimestampTableCellRendererComponent } from './data-renderers/timestamp/timestamp-table-cell-renderer.component';
-import { TableExpandedDetailRowCellRendererComponent } from './expanded-detail/table-expanded-detail-row-cell-renderer.component';
+import {
+  TimestampTableCellRendererComponent
+} from './data-renderers/timestamp/timestamp-table-cell-renderer.component';
+import {
+  TableExpandedDetailRowCellRendererComponent
+} from './expanded-detail/table-expanded-detail-row-cell-renderer.component';
 import { TableCellStateParser } from './state-parsers/table-cell-state-parser';
 import { TableCheckboxCellRendererComponent } from './state-renderers/checkbox/table-checkbox-cell-renderer.component';
 import { TableExpanderCellRendererComponent } from './state-renderers/expander/table-expander-cell-renderer.component';
@@ -53,7 +64,8 @@ export const TABLE_CELL_PARSERS = new InjectionToken<unknown[][]>('TABLE_CELL_PA
     FilterButtonModule,
     FilterModalModule,
     PopoverModule,
-    CopyToClipboardModule
+    CopyToClipboardModule,
+    XMoreModule
   ],
   exports: [
     TableHeaderCellRendererComponent,
