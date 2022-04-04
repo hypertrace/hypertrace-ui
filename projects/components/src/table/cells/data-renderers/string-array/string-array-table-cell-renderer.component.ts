9721cf21a24fd720de805f545e509c3fdaa8a1d7
Arjunlal B <arjun@traceable.ai>
2022-04-04 13:18:23 +0530

    fix: prettier fix

--- projects/components/src/table/cells/data-renderers/string-array/string-array-table-cell-renderer.component.ts
+++ projects/components/src/table/cells/data-renderers/string-array/string-array-table-cell-renderer.component.ts
@@ -13,7 +13,7 @@ import { TableCellAlignmentType } from '../../types/table-cell-alignment-type';
   template: `
     <div class="string-array-cell" [htTooltip]="summaryTooltip">
       <span class="first-item">{{ this.value[0] | htDisplayString }}</span>
-      <ht-x-more [count]="(this.value | slice:1).length" displayStyle="${XMoreDisplay.Gray}"></ht-x-more>
+      <ht-x-more [count]="(this.value | slice: 1).length" displayStyle="${XMoreDisplay.Gray}"></ht-x-more>
 
       <ng-template #summaryTooltip>
         <div *ngFor="let value of this.value">{{ value }}</div>
