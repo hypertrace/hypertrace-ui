17f97a588377babdbeae954e3862a1b743bc8e7e
Arjunlal B <arjun@traceable.ai>
2022-04-06 14:59:47 +0530

    fix: define a service to provide global header height

--- projects/common/src/constants/application-constants.ts
+++ projects/common/src/constants/application-constants.ts
@@ -1,7 +1,3 @@
-import { InjectionToken } from '@angular/core';
-
-export const GLOBAL_HEADER_HEIGHT = new InjectionToken<string>('Global Header Height');
-
 export const enum ApplicationFeature {
   PageTimeRange = 'ui.page-time-range'
 }
