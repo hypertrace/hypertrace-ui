415dd3dc1b87cf2bd0b3739fb00787b46328c962
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2022-04-07 15:16:20 -0700

    fix: displaying search when > 5 options
    - also making collapsible panel header to toggle

--- projects/components/src/collapsible-sidebar/collapsible-sidebar.module.ts
+++ projects/components/src/collapsible-sidebar/collapsible-sidebar.module.ts
@@ -1,10 +1,11 @@
+import { EventBlockerModule } from './../event-blocker/event-blocker.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconModule } from '../icon/icon.module';
 import { CollapsibleSidebarComponent } from './collapsible-sidebar.component';
 
 @NgModule({
-  imports: [CommonModule, IconModule],
+  imports: [CommonModule, IconModule, EventBlockerModule],
   declarations: [CollapsibleSidebarComponent],
   exports: [CollapsibleSidebarComponent]
 })
