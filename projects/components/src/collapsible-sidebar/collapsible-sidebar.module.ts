d93bf9707c5f4ad496fdb48879169b26f60999d1
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2022-04-11 14:19:57 -0700

    refactor: fix lint

--- projects/components/src/collapsible-sidebar/collapsible-sidebar.module.ts
+++ projects/components/src/collapsible-sidebar/collapsible-sidebar.module.ts
@@ -1,7 +1,7 @@
-import { EventBlockerModule } from './../event-blocker/event-blocker.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconModule } from '../icon/icon.module';
+import { EventBlockerModule } from './../event-blocker/event-blocker.module';
 import { CollapsibleSidebarComponent } from './collapsible-sidebar.component';
 
 @NgModule({
