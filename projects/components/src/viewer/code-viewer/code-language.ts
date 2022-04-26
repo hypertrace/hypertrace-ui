c439b8906b26f6837b08b415b98fb334134dc276
Sandeep Sharma <78212931+itssharmasandeep@users.noreply.github.com>
2022-04-27 01:31:16 +0530

    Revert "feat: syntax highlighting for code viewer (#1570)" (#1572)
    This reverts commit 6ee08fff40e3187a5226ab015096740660679fb5.

--- projects/components/src/viewer/code-viewer/code-language.ts
+++
@@ -1,4 +0,0 @@
-export const enum CodeLanguage {
-  Json = 'json',
-  Yaml = 'yaml'
-}
--- projects/components/src/viewer/code-viewer/code-viewer.component.ts
+++ projects/components/src/viewer/code-viewer/code-viewer.component.ts
@@ -6,17 +6,12 @@ import {
   Input,
   OnChanges,
   OnDestroy,
-  QueryList,
-  TemplateRef,
-  ViewChildren
+  TemplateRef
 } from '@angular/core';
-import { IconType } from '@hypertrace/assets-library';
 import { Color, TypedSimpleChanges } from '@hypertrace/common';
-import hljs from 'highlight.js';
 import { isEmpty } from 'lodash-es';
 import { of } from 'rxjs';
 import { DownloadFileMetadata } from '../../download-file/download-file-metadata';
-import { CodeLanguage } from './code-language';
 
 @Component({
   selector: 'ht-code-viewer',
@@ -41,8 +36,8 @@ import { CodeLanguage } from './code-language';
           ></ht-download-file>
         </div>
       </div>
-      <div id="code-viewer-content" class="content" [ngClass]="{ 'no-data': this.isCodeEmpty }">
-        <ng-container *ngIf="!this.isCodeEmpty; else noDataTemplate"
+      <div id="code-viewer-content" class="content">
+        <ng-container *ngIf="this.code"
           ><div class="line-numbers">
             <div
               *ngFor="let lineNumber of this.lineNumbers"
@@ -58,8 +53,14 @@ import { CodeLanguage } from './code-language';
               class="code-line"
               [ngClass]="{ 'line-highlight': this.isLineHighlighted(index) }"
             >
-              <pre class="code-line-text" [innerHtml]="codeLine"></pre>
-              <div #codeLineBackground class="code-line-background"></div>
+              <pre
+                class="code-line-text"
+                [innerHtml]="
+                  this.searchText
+                    ? (codeLine | htHighlight: { text: this.searchText, highlightType: 'mark' })
+                    : codeLine
+                "
+              ></pre>
             </div>
           </div>
           <ht-copy-to-clipboard
@@ -70,9 +71,6 @@ import { CodeLanguage } from './code-language';
             [text]="this.code"
           ></ht-copy-to-clipboard>
         </ng-container>
-        <ng-template #noDataTemplate>
-          <ht-message-display icon="${IconType.NoData}" [title]="this.noDataMessage"></ht-message-display>
-        </ng-template>
       </div>
     </div>
   `
@@ -82,16 +80,10 @@ export class CodeViewerComponent implements AfterViewInit, OnChanges, OnDestroy 
   public code: string = ''; // Pre-formatted code string
 
   @Input()
-  public language: CodeLanguage = CodeLanguage.Yaml; // Need a default language by highlight.js
-
-  @Input()
   public highlightText: string = ''; // To highlight the entire line
 
   @Input()
   public titleText: string = 'Code Viewer';
-
-  @Input()
-  public noDataMessage: string = 'No code available';
 
   @Input()
   public backgroundColor: string = Color.OffWhite;
@@ -120,13 +112,10 @@ export class CodeViewerComponent implements AfterViewInit, OnChanges, OnDestroy 
   @Input()
   public lineSplitter: RegExp = new RegExp('\r\n|\r|\n');
 
-  @ViewChildren('codeLineBackground', { read: ElementRef })
-  private readonly codeLineBackgroundElements!: QueryList<ElementRef>;
-
-  public codeTexts: string[] = [];
-  public codeLines: string[] = []; // HTML strings after syntax highlighting
+  public codeLines: string[] = [];
   public downloadCodeMetadata?: DownloadFileMetadata;
   public lineNumbers: number[] = [];
+  public searchText: string = '';
 
   private readonly domMutationObserver: MutationObserver = new MutationObserver(mutations =>
     this.onDomMutation(mutations)
@@ -139,9 +128,8 @@ export class CodeViewerComponent implements AfterViewInit, OnChanges, OnDestroy 
   }
 
   public ngOnChanges(changes: TypedSimpleChanges<this>): void {
-    if (changes.code || changes.codeLanguage) {
-      this.codeTexts = isEmpty(this.code) ? [] : this.code.split(this.lineSplitter);
-      this.codeLines = this.codeTexts.map(codeStr => hljs.highlight(codeStr, { language: this.language }).value);
+    if (changes.code) {
+      this.codeLines = isEmpty(this.code) ? [] : this.code.split(this.lineSplitter);
       this.lineNumbers = new Array(this.codeLines.length).fill(0).map((_, index) => index + 1);
     }
 
@@ -157,69 +145,14 @@ export class CodeViewerComponent implements AfterViewInit, OnChanges, OnDestroy 
     this.domMutationObserver.disconnect();
   }
 
-  public get isCodeEmpty(): boolean {
-    return isEmpty(this.code);
-  }
-
   public isLineHighlighted(lineNum: number): boolean {
     return (
       !isEmpty(this.highlightText) && this.codeLines[lineNum].toLowerCase().includes(this.highlightText.toLowerCase())
     );
   }
 
-  public onSearch(searchString: string): void {
-    // For case-insensitive search
-    const searchText = searchString.toLowerCase();
-    const codeTexts = this.codeTexts.map(text => text.toLowerCase());
-
-    // Remove existing child background elements
-    this.codeLineBackgroundElements.forEach(
-      codeLineBackgroundElement => (codeLineBackgroundElement.nativeElement.innerHTML = '')
-    );
-
-    const searchLen: number = searchText.length;
-    if (searchLen === 0) {
-      return;
-    }
-
-    const searchedPositions: Position[][] = Array.from(Array(codeTexts.length), () => []);
-
-    // Get all searched text positions
-    codeTexts.forEach((codeText, index) => {
-      for (let i = 0; i <= codeText.length - searchLen; i++) {
-        const searchedIndex = codeText.indexOf(searchText, i);
-        if (searchedIndex > -1) {
-          searchedPositions[index].push({ start: searchedIndex, end: searchedIndex + searchLen });
-          i = searchedIndex + searchLen - 1;
-        } else {
-          break;
-        }
-      }
-    });
-
-    // Add background elements for searched positions
-    this.codeLineBackgroundElements.forEach((codeLineBackgroundElement, index) => {
-      codeLineBackgroundElement.nativeElement.appendChild(this.getBackgroundElements(searchedPositions[index]));
-    });
-  }
-
-  // Background elements for search
-  private getBackgroundElements(positions: Position[]): DocumentFragment {
-    const backgroundElemDocFragment: DocumentFragment = document.createDocumentFragment();
-
-    positions.forEach(position => {
-      const backgroundElem = document.createElement('div');
-      backgroundElem.className = 'bg-searched';
-      backgroundElem.style.height = '100%';
-      backgroundElem.style.width = `${position.end - position.start}ch`;
-      backgroundElem.style.backgroundColor = Color.Yellow4;
-      backgroundElem.style.position = 'absolute';
-      backgroundElem.style.left = `${position.start}ch`;
-
-      backgroundElemDocFragment.appendChild(backgroundElem);
-    });
-
-    return backgroundElemDocFragment;
+  public onSearch(searchText: string): void {
+    this.searchText = searchText;
   }
 
   private observeDomMutations(): void {
@@ -236,16 +169,11 @@ export class CodeViewerComponent implements AfterViewInit, OnChanges, OnDestroy 
     const mutationType: MutationRecordType = mutations[0].type;
 
     if (mutationType === 'childList') {
-      const searchedElement: HTMLElement = this.element.nativeElement.querySelector('.bg-searched');
-      searchedElement?.scrollIntoView();
+      const markElement: HTMLElement = this.element.nativeElement.querySelector('mark');
+      markElement?.scrollIntoView();
     } else if (mutationType === 'attributes') {
       const highlightedCodeLineElement: HTMLElement = this.element.nativeElement.querySelector('.line-highlight');
       highlightedCodeLineElement?.scrollIntoView();
     }
   }
-}
-
-interface Position {
-  start: number;
-  end: number;
 }
