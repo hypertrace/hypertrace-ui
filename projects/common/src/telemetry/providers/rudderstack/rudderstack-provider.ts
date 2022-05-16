2bb3c062aad0963a2e9cb62b63a70b76a088727a
Jay Bhatt <56739130+jaywalker21@users.noreply.github.com>
2022-05-16 13:46:37 +0530

    Load `email` and `name` for analytics purpose from `user-info` endpoint for HyperTrace (#36)
    * feat(analytics): load user email when calling identify event
    
    * fix: read name and email from response not user traits
    
    * chore: remove log for success and add log for error case
    
    * chore(nginx): updated configuration to serve the user email and name (OBSV-708)

--- projects/common/src/telemetry/providers/rudderstack/rudderstack-provider.ts
+++ projects/common/src/telemetry/providers/rudderstack/rudderstack-provider.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dictionary } from '../../../utilities/types/types';
 
+import { HttpClient } from '@angular/common/http';
 import { apiObject, identify, load, page, track } from 'rudder-sdk-js';
 import { TelemetryProviderConfig, UserTelemetryProvider, UserTraits } from '../../telemetry';
 
@@ -10,6 +11,8 @@ export interface RudderStackConfig extends TelemetryProviderConfig {
 
 @Injectable({ providedIn: 'root' })
 export class RudderStackTelemetry implements UserTelemetryProvider<RudderStackConfig> {
+  public constructor(private readonly http: HttpClient) {}
+
   public initialize(config: RudderStackConfig): void {
     try {
       load(config.writeKey, config.orgId, { configUrl: config.orgId });
@@ -23,8 +26,16 @@ export class RudderStackTelemetry implements UserTelemetryProvider<RudderStackCo
     }
   }
 
-  public identify(userTraits: UserTraits): void {
-    identify(undefined, userTraits as apiObject);
+  public identify(): void {
+    this.http.get<UserTraits>('/user-info').subscribe(
+      (data: UserTraits) => {
+        identify(data.email, { email: data.email, name: data.name });
+      },
+      error => {
+        // tslint:disable-next-line: no-console
+        console.error('something went wrong in identify ', error);
+      }
+    );
   }
 
   public trackEvent(name: string, eventData: Dictionary<unknown>): void {
