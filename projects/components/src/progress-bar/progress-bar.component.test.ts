a6498244ac21e632262e1d6f7b1f11beeaa8a3e2
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2022-05-20 12:57:24 +0530

    fix: add tests for progress bar component

--- projects/components/src/progress-bar/progress-bar.component.test.ts
+++ projects/components/src/progress-bar/progress-bar.component.test.ts
@@ -1,3 +1,4 @@
+import { Color } from '@hypertrace/common';
 import { createComponentFactory } from '@ngneat/spectator/jest';
 import { ProgressBarComponent } from './progress-bar.component';
 
@@ -8,7 +9,14 @@ describe('Progress Bar Component', () => {
   });
 
   test('should render everything correctly', () => {
-    const spectator = createComponent();
-    expect(spectator.query('.progress-bar')).toExist();
+    const spectator = createComponent({
+      props: {
+        progress: 40
+      }
+    });
+    expect(spectator.query('.progress')).toExist();
+    expect(spectator.query('.progress-bar')).toHaveStyle({ backgroundColor: Color.Gray3 });
+    expect(spectator.query('.progress-bar .bar')).toHaveStyle({ backgroundColor: Color.Blue4, width: '40%' });
+    expect(spectator.query('.progress-percentage')).toHaveText('40%');
   });
 });
