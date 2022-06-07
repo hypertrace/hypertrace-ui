95e503e3aeaeb7fd317f2178778082863900785d
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2022-06-07 18:44:40 +0530

    fix: removed unwanted imports

--- projects/components/src/tree-with-checkboxes/tree-with-checkboxes.module.ts
+++ projects/components/src/tree-with-checkboxes/tree-with-checkboxes.module.ts
@@ -1,30 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TreeWithCheckboxesComponent } from './tree-with-checkboxes.component';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { IconModule } from '../icon/icon.module';
-import { TraceCheckboxModule } from '../checkbox/checkbox.module';
+import { MatTreeModule } from '@angular/material/tree';
 import { ButtonModule } from '../button/button.module';
-import { InputModule } from '../input/input.module';
+import { TraceCheckboxModule } from '../checkbox/checkbox.module';
+import { IconModule } from '../icon/icon.module';
 import { LabelModule } from '../label/label.module';
+import { TreeWithCheckboxesComponent } from './tree-with-checkboxes.component';
 
 @NgModule({
   declarations: [TreeWithCheckboxesComponent],
   exports: [TreeWithCheckboxesComponent],
-  imports: [
-    CommonModule,
-    MatTreeModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatIconModule,
-    IconModule,
-    TraceCheckboxModule,
-    ButtonModule,
-    InputModule,
-    LabelModule
-  ]
+  imports: [CommonModule, MatTreeModule, MatFormFieldModule, IconModule, TraceCheckboxModule, ButtonModule, LabelModule]
 })
 export class TreeWithCheckboxesModule {}
