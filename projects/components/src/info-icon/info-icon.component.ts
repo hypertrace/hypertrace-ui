d4ba8f1bbef088a1e5424271a391bd0ce252c3c7
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2022-06-07 16:06:05 +0530

    fix: addressing review comments

--- projects/components/src/info-icon/info-icon.component.ts
+++ projects/components/src/info-icon/info-icon.component.ts
@@ -7,12 +7,8 @@ import { IconSize } from '../icon/icon-size';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <ng-container *ngIf="this.info"
-      ><ht-icon icon="${IconType.Info}" [size]="this.iconSize" [htTooltip]="infoTooltipTemplate"></ht-icon>
-      <ng-template #infoTooltipTemplate>
-        <ng-container *ngTemplateOutlet="this.isInfoAString ? defaultInfoTooltipTemplate : this.info"></ng-container>
-      </ng-template>
-      <ng-template #defaultInfoTooltipTemplate> {{ this.info }}</ng-template></ng-container
-    >
+      ><ht-icon icon="${IconType.Info}" [size]="this.iconSize" [htTooltip]="this.info"></ht-icon>
+    </ng-container>
   `
 })
 export class InfoIconComponent {
@@ -21,8 +17,4 @@ export class InfoIconComponent {
 
   @Input()
   public iconSize: IconSize = IconSize.Small;
-
-  public get isInfoAString(): boolean {
-    return typeof this.info === 'string';
-  }
 }
