2990f6fd862cd908bccb360a8d48021a383d9779
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2022-06-07 13:39:49 +0530

    fix: updated types.ts

--- projects/components/src/tree-with-checkboxes/types.ts
+++ projects/components/src/tree-with-checkboxes/types.ts
@@ -1,15 +1,13 @@
 /** Flat to-do item node with expandable and level information */
 export interface TodoItemFlatNode {
-    item: string;
-    level: number;
-    expandable: boolean;
+  item: string;
+  level: number;
+  expandable: boolean;
 }
 
-/**
- * Node for to-do item
+/* Node for to-do item
  */
- export interface TodoItemNode {
-    children?: TodoItemNode[];
-    item: string;
-  }
-  
+export interface TodoItemNode {
+  children?: TodoItemNode[];
+  item: string;
+}
