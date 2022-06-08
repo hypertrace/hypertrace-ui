6a60575c923766f2d9e27ccdb627d67118c79daa
Sandeep Sharma <78212931+itssharmasandeep@users.noreply.github.com>
2022-06-08 11:44:57 +0530

    feat: info icon component (#1666)

--- projects/components/src/tabs/content/tab.module.ts
+++ projects/components/src/tabs/content/tab.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
+import { InfoIconModule } from '../../info-icon/info-icon.module';
 import { LabelTagModule } from '../../label-tag/label-tag.module';
 import { TabGroupComponent } from './tab-group.component';
 import { TabComponent } from './tab/tab.component';
@@ -9,6 +10,6 @@ import { TabComponent } from './tab/tab.component';
 @NgModule({
   declarations: [TabGroupComponent, TabComponent],
   exports: [TabGroupComponent, TabComponent],
-  imports: [MatTabsModule, CommonModule, RouterModule, LabelTagModule]
+  imports: [MatTabsModule, CommonModule, RouterModule, LabelTagModule, InfoIconModule]
 })
 export class TabModule {}
