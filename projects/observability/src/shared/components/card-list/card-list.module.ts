02d047ae34d8cd7a14903c22dc06e292a587ea06
Ankit Das <ankit.das@traceable.ai>
2022-06-08 16:40:32 +0530

    refactor: rename TraceCheckboxModule to CheckboxModule (#1669)
    * refactor: rename TraceCheckboxModule to CheckboxModule
    
    Co-authored-by: Ankit Das <ankitdas@Ankits-MacBook-Pro.local>

--- projects/observability/src/shared/components/card-list/card-list.module.ts
+++ projects/observability/src/shared/components/card-list/card-list.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { TraceCheckboxModule } from '@hypertrace/components';
+import { CheckboxModule } from '@hypertrace/components';
 import { CardListComponent } from './card-list.component';
 import { CardContainerComponent } from './container/card-container.component';
 
 @NgModule({
-  imports: [CommonModule, TraceCheckboxModule],
+  imports: [CommonModule, CheckboxModule],
   declarations: [CardListComponent, CardContainerComponent],
   exports: [CardListComponent, CardContainerComponent]
 })
