1a9afbbff7c9f648cbed9ec03f5b5e2067edeee6
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2022-06-09 13:19:40 +0530

    fix: renamed TraceCheckboxModule with CheckboxModule in TreeWithCheckboxesModule

--- projects/components/src/tree-with-checkboxes/tree-with-checkboxes.module.ts
+++ projects/components/src/tree-with-checkboxes/tree-with-checkboxes.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTreeModule } from '@angular/material/tree';
 import { ButtonModule } from '../button/button.module';
-import { TraceCheckboxModule } from '../checkbox/checkbox.module';
+import { CheckboxModule } from '../checkbox/checkbox.module';
 import { IconModule } from '../icon/icon.module';
 import { LabelModule } from '../label/label.module';
 import { TreeWithCheckboxesComponent } from './tree-with-checkboxes.component';
@@ -11,6 +11,6 @@ import { TreeWithCheckboxesComponent } from './tree-with-checkboxes.component';
 @NgModule({
   declarations: [TreeWithCheckboxesComponent],
   exports: [TreeWithCheckboxesComponent],
-  imports: [CommonModule, MatTreeModule, MatFormFieldModule, IconModule, TraceCheckboxModule, ButtonModule, LabelModule]
+  imports: [CommonModule, MatTreeModule, MatFormFieldModule, IconModule, CheckboxModule, ButtonModule, LabelModule]
 })
 export class TreeWithCheckboxesModule {}
