2695bd0cf0cb7c88d274f3a1c1c2e82fe8dc3c08
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2022-06-21 22:57:38 +0530

    feat: rename test name

--- projects/common/src/utilities/validators/regex-validator.test.ts
+++ projects/common/src/utilities/validators/regex-validator.test.ts
@@ -4,11 +4,11 @@ const VALID_REGEX = ['2+2', '(2+2) * 3*9'];
 const INVALID_REGEX = ['\\', '['];
 describe('Regex validator', () => {
   test('can validate regex lists correctly', () => {
-    VALID_REGEX.forEach(domain => {
-      expect(isValidRegex(domain)).toBeTruthy();
+    VALID_REGEX.forEach(regex => {
+      expect(isValidRegex(regex)).toBeTruthy();
     });
-    INVALID_REGEX.forEach(domain => {
-      expect(isValidRegex(domain)).toBeFalsy();
+    INVALID_REGEX.forEach(regex => {
+      expect(isValidRegex(regex)).toBeFalsy();
     });
   });
 });
