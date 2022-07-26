9355d18129d2575a02099fbbf2025eeee439c4a6
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2022-07-25 23:55:07 -0400

    refactor: adding progress bar and loader to file display

--- projects/components/src/file-upload/file-display/file-display.component.ts
+++ projects/components/src/file-upload/file-display/file-display.component.ts
@@ -2,55 +2,62 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 import { IconType } from '@hypertrace/assets-library';
 import { Color } from '@hypertrace/common';
 import { IconSize } from '../../icon/icon-size';
-import { FileUploadEvent, FileUploadEventType, FileUploadFailureEvent } from '../file-upload.service';
+import {
+  FileUploadEvent,
+  FileUploadEventType,
+  FileUploadFailureEvent,
+  FileUploadProgressEvent
+} from '../file-upload.service';
 
 @Component({
   selector: 'ht-file-display',
   styleUrls: ['./file-display.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div
-      *ngIf="this.file"
-      class="file-display"
-      [ngClass]="{ error: this.latestUploadEvent?.type === '${FileUploadEventType.Failure}' }"
-    >
-      <ht-icon class="note-icon" icon="${IconType.Note}" size="${IconSize.Medium}" color="${Color.Blue4}"></ht-icon>
-      <div class="file-info">
-        <div class="basic-detail">
-          <div class="file-name" [htTooltip]="this.file.name">{{ this.file.name }}</div>
-          <div class="file-size">{{ this.file.size | htDisplayFileSize }}</div>
+    <div class="file-display-container">
+      <ht-progress-bar
+        *ngIf="this.latestUploadEvent?.type === '${FileUploadEventType.Progress}'"
+        class="progress-bar"
+        [progress]="this.getProgress()"
+      ></ht-progress-bar>
+      <div
+        *ngIf="this.file"
+        class="file-display"
+        [ngClass]="{ error: this.latestUploadEvent?.type === '${FileUploadEventType.Failure}' }"
+      >
+        <ht-icon class="note-icon" icon="${IconType.Note}" size="${IconSize.Medium}" color="${Color.Blue4}"></ht-icon>
+        <div class="file-info">
+          <div class="basic-detail">
+            <div class="file-name" [htTooltip]="this.file.name">{{ this.file.name }}</div>
+            <div class="file-size">{{ this.file.size | htDisplayFileSize }}</div>
+          </div>
         </div>
-      </div>
-      <ng-container *ngIf="this.latestUploadEvent">
-        <ng-container [ngSwitch]="this.latestUploadEvent?.type"
-          ><ng-container *ngSwitchCase="'${FileUploadEventType.Success}'"
-            ><ht-icon class="success-icon" icon="${IconType.CheckCircle}" color="${Color.Green5}"></ht-icon
-          ></ng-container>
-          <ng-container *ngSwitchCase="'${FileUploadEventType.Failure}'"
-            ><ht-icon
-              class="failure-icon"
-              icon="${IconType.Alert}"
-              color="${Color.Red6}"
-              [htTooltip]="this.getError()"
-            ></ht-icon>
-          </ng-container>
-          <ng-container *ngSwitchCase="'${FileUploadEventType.Progress}'"
-            ><ht-icon
-              class="progress-icon"
-              icon="${IconType.Unknown}"
-              color="${Color.Blue4}"
-              htTooltip="Progress"
-            ></ht-icon>
+        <ng-container *ngIf="this.latestUploadEvent">
+          <ng-container [ngSwitch]="this.latestUploadEvent?.type"
+            ><ng-container *ngSwitchCase="'${FileUploadEventType.Success}'"
+              ><ht-icon class="success-icon" icon="${IconType.CheckCircle}" color="${Color.Green5}"></ht-icon
+            ></ng-container>
+            <ng-container *ngSwitchCase="'${FileUploadEventType.Failure}'"
+              ><ht-icon
+                class="failure-icon"
+                icon="${IconType.Alert}"
+                color="${Color.Red6}"
+                [htTooltip]="this.getError()"
+              ></ht-icon>
+            </ng-container>
+            <ng-container *ngSwitchCase="'${FileUploadEventType.Progress}'">
+              <ht-loader class="progress-icon" htTooltip="Progress"></ht-loader>
+            </ng-container>
           </ng-container>
         </ng-container>
-      </ng-container>
-      <ht-icon
-        class="delete-icon"
-        icon="${IconType.CloseCircleFilled}"
-        size="${IconSize.Small}"
-        color="${Color.Gray9}"
-        (click)="this.onDeleteClick()"
-      ></ht-icon>
+        <ht-icon
+          class="delete-icon"
+          icon="${IconType.CloseCircleFilled}"
+          size="${IconSize.Small}"
+          color="${Color.Gray9}"
+          (click)="this.onDeleteClick()"
+        ></ht-icon>
+      </div>
     </div>
   `
 })
@@ -70,5 +77,9 @@ export class FileDisplayComponent {
 
   public getError(): string {
     return (this.latestUploadEvent as FileUploadFailureEvent)?.error ?? '';
+  }
+
+  public getProgress(): number {
+    return (this.latestUploadEvent as FileUploadProgressEvent)?.progress ?? 0;
   }
 }
