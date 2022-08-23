ff857802737e7f5c62d5285616e681f8aebc48e0
Shreyansh Sahu <shreyansh.sahu@traceable.ai>
2022-08-23 17:39:17 +0530

    feat: added new stepper component (#1791)
    * feat: added new stepper component

---
+++ projects/components/src/stepper/stepper.component.test.ts
@@ -0,0 +1,108 @@
+import { MatStepper, MatStepperModule } from '@angular/material/stepper';
+import { MemoizeModule } from '@hypertrace/common';
+import { LoadAsyncModule, StepperComponent } from '@hypertrace/components';
+import { createHostFactory, Spectator } from '@ngneat/spectator/jest';
+import { MockComponents } from 'ng-mocks';
+import { ButtonComponent } from '../button/button.component';
+import { IconComponent } from '../icon/icon.component';
+import { LabelComponent } from '../label/label.component';
+import { StepperTabComponent } from './stepper-tab.component';
+
+describe('Stepper Component', () => {
+  let spectator: Spectator<StepperComponent>;
+
+  const createHost = createHostFactory({
+    component: StepperComponent,
+    declarations: [MockComponents(ButtonComponent, LabelComponent, IconComponent), StepperTabComponent],
+    imports: [MemoizeModule, MatStepperModule, LoadAsyncModule],
+    shallow: true
+  });
+
+  test('should render stepper properly', () => {
+    spectator = createHost(
+      `<ht-stepper class="stepper">
+          <ht-stepper-tab label="Hello"> Hello World!</ht-stepper-tab>
+          <ht-stepper-tab label="Yo"> Hey!</ht-stepper-tab>
+          <ht-stepper-tab label="Kem cho"> Test!</ht-stepper-tab>
+        </ht-stepper>`
+    );
+
+    expect(spectator.query('.stepper')).toExist();
+    expect(spectator.query('.action-buttons')).toExist();
+    const tabLabels = spectator.queryAll(LabelComponent);
+    expect(tabLabels[1].label).toBe('Yo');
+    expect(spectator.query('.back-next')).toExist();
+    expect(spectator.query('.cancel')).toExist();
+    expect(spectator.query('.next')).toExist();
+    expect(spectator.query('.back')).not.toExist();
+  });
+
+  test('should switch between tabs correctly', () => {
+    spectator = createHost(
+      `<ht-stepper class="stepper">
+            <ht-stepper-tab label="Hello"> Hello World!</ht-stepper-tab>
+            <ht-stepper-tab label="Yo"> Hey!</ht-stepper-tab>
+            <ht-stepper-tab label="Kem cho"> Test!</ht-stepper-tab>
+          </ht-stepper>`
+    );
+
+    spectator.click('.next');
+    expect(spectator.query(MatStepper)?.selectedIndex).toBe(1);
+    expect(spectator.query('.action-buttons')).toExist();
+    expect(spectator.query('.back')).toExist();
+
+    spectator.click('.next');
+    expect(spectator.query(MatStepper)?.selectedIndex).toBe(2);
+    expect(spectator.query('.submit')).toExist();
+    expect(spectator.query('.next')).not.toExist();
+
+    spectator.click('.back');
+    expect(spectator.query(MatStepper)?.selectedIndex).toBe(1);
+  });
+
+  test('should disable next button correctly on tab completed state', () => {
+    spectator = createHost(
+      `<ht-stepper class="stepper" [isLinear]="isLinear">
+            <ht-stepper-tab label="Hello" [completed]="tabOneStatus"> Hello World!</ht-stepper-tab>
+            <ht-stepper-tab label="Yo" [completed]="tabTwoStatus"> Hey!</ht-stepper-tab>
+            <ht-stepper-tab label="Kem cho" [completed]="tabThreeStatus"> Test!</ht-stepper-tab>
+          </ht-stepper>`,
+      {
+        hostProps: {
+          isLinear: true,
+          tabOneStatus: false,
+          tabTwoStatus: false,
+          tabThreeStatus: false
+        }
+      }
+    );
+
+    const actionButtons = spectator.queryAll(ButtonComponent);
+    expect(spectator.query('.next')).toExist();
+    expect(actionButtons[1].disabled).toBe(true);
+  });
+
+  test('should disable submit button correctly on tab completed state', () => {
+    spectator = createHost(
+      `<ht-stepper class="stepper" [isLinear]="isLinear">
+            <ht-stepper-tab label="Hello" [completed]="tabOneStatus"> Hello World!</ht-stepper-tab>
+            <ht-stepper-tab label="Yo" [completed]="tabTwoStatus"> Hey!</ht-stepper-tab>
+            <ht-stepper-tab label="Kem cho" [completed]="tabThreeStatus"> Test!</ht-stepper-tab>
+          </ht-stepper>`,
+      {
+        hostProps: {
+          isLinear: true,
+          tabOneStatus: true,
+          tabTwoStatus: true,
+          tabThreeStatus: false
+        }
+      }
+    );
+
+    spectator.click('.next');
+    spectator.click('.next');
+    const actionButtons = spectator.queryAll(ButtonComponent);
+    expect(spectator.query('.submit')).toExist();
+    expect(actionButtons[2].disabled).toBe(true);
+  });
+});
---
+++ projects/components/src/stepper/stepper.component.ts
@@ -0,0 +1,119 @@
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList
+} from '@angular/core';
+import { MatStepper } from '@angular/material/stepper';
+import { queryListAndChanges$ } from '@hypertrace/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ButtonRole, ButtonStyle } from '../button/button';
+import { IconSize } from '../icon/icon-size';
+import { StepperTabComponent } from './stepper-tab.component';
+
+@Component({
+  selector: 'ht-stepper',
+  template: `
+    <div class="stepper" *htLoadAsync="this.steps$ as steps">
+      <mat-stepper [orientation]="this.orientation" [linear]="this.isLinear" #stepper>
+        <ng-container *ngFor="let step of steps">
+          <mat-step [completed]="step.completed" [stepControl]="step.stepControl" [optional]="step.optional">
+            <ng-template matStepLabel>
+              <ht-label class="header-label" [label]="step.label"></ht-label>
+            </ng-template>
+            <ng-container *ngTemplateOutlet="step.content"></ng-container>
+            <ng-template matStepperIcon="edit">
+              <ht-icon [icon]="step.icon" size="${IconSize.ExtraSmall}"> </ht-icon>
+            </ng-template>
+          </mat-step>
+        </ng-container>
+      </mat-stepper>
+      <div class="action-buttons">
+        <ht-button
+          class="cancel"
+          label="Cancel"
+          (click)="this.cancelled.emit()"
+          role="${ButtonRole.Tertiary}"
+        ></ht-button>
+        <div class="back-next">
+          <ht-button
+            class="back"
+            *ngIf="stepper.selectedIndex !== 0"
+            display="${ButtonStyle.Outlined}"
+            label="Back"
+            (click)="stepper.previous()"
+          ></ht-button>
+          <ht-button
+            class="next"
+            *ngIf="stepper.selectedIndex !== steps.length - 1"
+            label="Next"
+            (click)="stepper.next()"
+            [disabled]="this.isNextDisabled(stepper)"
+          ></ht-button>
+          <ht-button
+            class="submit"
+            *ngIf="stepper.selectedIndex === steps.length - 1"
+            label="Submit"
+            (click)="this.submitted.emit()"
+            [disabled]="this.isSubmitDisabled(stepper)"
+          ></ht-button>
+        </div>
+      </div>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./stepper.component.scss']
+})
+export class StepperComponent implements AfterContentInit {
+  // TODO: Fix support for custom icon, currently broken.
+
+  @ContentChildren(StepperTabComponent)
+  private readonly steps!: QueryList<StepperTabComponent>;
+
+  // If stepper is Linear, then we can navigate to a tab only if previous all tabs are at complete state.
+  @Input()
+  public isLinear: boolean = false;
+
+  // Currently we only support horizontal orientation.
+  @Input()
+  public orientation: StepperOrientation = StepperOrientation.Horizontal;
+
+  @Output()
+  public readonly submitted: EventEmitter<void> = new EventEmitter();
+
+  @Output()
+  public readonly cancelled: EventEmitter<void> = new EventEmitter();
+
+  public steps$?: Observable<StepperTabComponent[]>;
+
+  public ngAfterContentInit(): void {
+    this.steps$ = queryListAndChanges$(this.steps).pipe(map(list => list.toArray()));
+  }
+
+  // TODO: can be made a directive
+  public isNextDisabled(stepper: MatStepper): boolean {
+    const selectedStep = stepper.steps.get(stepper.selectedIndex);
+    const formValid = selectedStep?.stepControl?.valid;
+    const stepCompleted = selectedStep?.completed;
+
+    return this.isLinear && !stepCompleted && !formValid;
+  }
+
+  // TODO: can be made a directive
+  public isSubmitDisabled(stepper: MatStepper): boolean {
+    const selectedStep = stepper.steps.get(stepper.selectedIndex);
+    const formValid = selectedStep?.stepControl?.valid;
+    const stepCompleted = selectedStep?.completed;
+
+    return !stepCompleted && !formValid;
+  }
+}
+
+export enum StepperOrientation {
+  Horizontal = 'horizontal'
+}
