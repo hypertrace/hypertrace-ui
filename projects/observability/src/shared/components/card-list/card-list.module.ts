e780aa47471eb3a44d1293178894cfda7f68205e
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2022-08-23 14:21:54 +0530

    fix: remove unwanted code

--- projects/observability/src/shared/components/card-list/card-list.module.ts
+++ projects/observability/src/shared/components/card-list/card-list.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CheckboxModule, TooltipModule } from '@hypertrace/components';
+import { CheckboxModule } from '@hypertrace/components';
 import { CardListComponent } from './card-list.component';
 import { CardContainerComponent } from './container/card-container.component';
 
 @NgModule({
-  imports: [CommonModule, CheckboxModule, TooltipModule],
+  imports: [CommonModule, CheckboxModule],
   declarations: [CardListComponent, CardContainerComponent],
   exports: [CardListComponent, CardContainerComponent]
 })
