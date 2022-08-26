3e3253c5f7ed9b9e7564d845db98b89c214e78ae
Patricio Albizu <patricioalbizu@Patricios-MacBook-Pro.local>
2022-08-26 19:58:13 -0300

    fix: improving code

--- projects/observability/src/shared/components/explore-query-editor/explore-visualization-builder.ts
+++ projects/observability/src/shared/components/explore-query-editor/explore-visualization-builder.ts
@@ -238,12 +238,14 @@ export class ExploreVisualizationBuilder implements OnDestroy {
   private encodeGroupBy(groupBy: GraphQlGroupBy): GraphQlGroupBy {
     return {
       ...groupBy,
-      keyExpressions: [
-        {
-          ...groupBy.keyExpressions[0],
-          subpath: camelCase(groupBy.keyExpressions[0].subpath)
-        }
-      ]
+      keyExpressions: groupBy.keyExpressions[0]
+        ? [
+            {
+              ...groupBy.keyExpressions[0],
+              subpath: groupBy.keyExpressions[0].subpath && camelCase(groupBy.keyExpressions[0].subpath)
+            }
+          ]
+        : []
     };
   }
 }
