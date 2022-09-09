dc7b5eb82036e26d6d8e5bee41aea3f626907b10
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2022-09-09 12:03:54 +0530

    feat(dynamic-component): add method to attach component to given view container

--- projects/common/src/utilities/angular/dynamic-component.service.test.ts
+++ projects/common/src/utilities/angular/dynamic-component.service.test.ts
@@ -1,9 +1,19 @@
-import { Component, Inject, InjectionToken, Injector } from '@angular/core';
-import { createComponentFactory } from '@ngneat/spectator/jest';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  InjectionToken,
+  Injector,
+  ViewContainerRef
+} from '@angular/core';
+import { fakeAsync } from '@angular/core/testing';
+import { createComponentFactory, createServiceFactory } from '@ngneat/spectator/jest';
 import { DynamicComponentService } from './dynamic-component.service';
 
 describe('Dynamic component service', () => {
   const injectionToken = new InjectionToken<string>('test token');
+
   @Component({
     // tslint:disable-next-line: component-selector
     selector: 'dynamic-component',
@@ -36,5 +46,57 @@ describe('Dynamic component service', () => {
     expect(componentRef.instance).toBeInstanceOf(DynamicComponent);
     spectator.detectChanges(); // Need a cycle to update the binding
     expect(spectator.element).toHaveText('injected value');
+  });
+
+  describe('Test attachComponentToViewContainer method', () => {
+    @Component({
+      selector: 'trace-mock-component',
+      template: `
+        <div class="title">
+          {{ this.title }}
+        </div>
+      `,
+      changeDetection: ChangeDetectionStrategy.OnPush
+    })
+    class MockComponent {
+      public title: string = '';
+
+      public constructor(public readonly vcr: ViewContainerRef, public readonly cdr: ChangeDetectorRef) {}
+    }
+
+    const createService = createServiceFactory({
+      service: DynamicComponentService
+    });
+
+    const createComponent = createComponentFactory({
+      component: MockComponent
+    });
+    test('should render the component correctly with the props passed', fakeAsync(() => {
+      const spectator = createService();
+      const mockComponent = createComponent();
+      const component = spectator.service.attachComponentToViewContainer<MockComponent>({
+        component: MockComponent,
+        vcr: mockComponent.component.vcr,
+        props: {
+          title: 'test'
+        }
+      });
+
+      mockComponent.tick();
+      expect(component).toBeDefined();
+      const compElRef = component?.location.nativeElement as HTMLDivElement;
+      expect(compElRef.textContent?.trim()).toEqual('test');
+    }));
+
+    test('should return undefined if no vcr provided', () => {
+      const spectator = createService();
+      const component = spectator.service.attachComponentToViewContainer<MockComponent>({
+        component: MockComponent,
+        props: {
+          title: 'test'
+        }
+      });
+      expect(component).toBeUndefined();
+    });
   });
 });
--- projects/common/src/utilities/angular/dynamic-component.service.ts
+++ projects/common/src/utilities/angular/dynamic-component.service.ts
@@ -6,11 +6,16 @@ import {
   Injectable,
   Injector,
   StaticProvider,
-  Type
+  Type,
+  ViewContainerRef
 } from '@angular/core';
+import { isEmpty, isNil } from 'lodash-es';
+import { Dictionary } from '../types/types';
 
 @Injectable({ providedIn: 'root' })
 export class DynamicComponentService {
+  public constructor(private readonly componentFactoryResolver: ComponentFactoryResolver) {}
+
   public insertComponent<C>(
     container: Element,
     componentClass: Type<C>,
@@ -29,4 +34,31 @@ export class DynamicComponentService {
 
     return domPortalOutlet.attach(componentPortal);
   }
+
+  /**
+   * Dynamically create a component and insert it into the view container. Props passed
+   * to the component will be set on the component instance.
+   *
+   * @returns ComponentRef or undefined if the component could not be created
+   */
+  public attachComponentToViewContainer<T>(opts: DynamicComponentOptions<T>): ComponentRef<T> | undefined {
+    if (!isNil(opts.vcr) && !isNil(opts.component)) {
+      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(opts.component);
+
+      const componentRef = opts.vcr.createComponent(componentFactory);
+      if (!isEmpty(opts.props) && !isNil(componentRef)) {
+        Object.assign(componentRef.instance, opts.props);
+      }
+
+      return componentRef;
+    }
+
+    return undefined;
+  }
+}
+
+export interface DynamicComponentOptions<T> {
+  component: Type<T>;
+  vcr?: ViewContainerRef;
+  props?: Dictionary<unknown>;
 }
