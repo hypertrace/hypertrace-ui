3cd06c88c3f657f0e81147c861877623d5736a9b
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2022-09-12 21:04:52 +0530

    feat(dynamic-component): add method to attach component to given view… (#1813)
    * feat(dynamic-component): add method to attach component to given view container
    
    * chore: lint fix
    
    * refactor(dynamic-service): use injectors and providers instead of prop assignment
    
    * feat(dynamic-component): use existing method

--- projects/common/src/utilities/angular/dynamic-component.service.ts
+++ projects/common/src/utilities/angular/dynamic-component.service.ts
@@ -6,7 +6,8 @@ import {
   Injectable,
   Injector,
   StaticProvider,
-  Type
+  Type,
+  ViewContainerRef
 } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
@@ -28,5 +29,26 @@ export class DynamicComponentService {
     const componentPortal = new ComponentPortal(componentClass, undefined, componentInjector);
 
     return domPortalOutlet.attach(componentPortal);
+  }
+
+  /**
+   * Dynamically create a component and insert it into the view container. Data and services can be provided to the dynamic
+   * component via `providers`
+   *
+   * **Note**: The provided view container ref should be a proper DOM Node (`div`, `section`, etc) and not something like
+   * `ng-container`
+   * @returns ComponentRef or undefined if the component could not be created
+   */
+  public insertComponentToViewContainer<C>(
+    componentClass: Type<C>,
+    viewContainerRef: ViewContainerRef,
+    providers: StaticProvider[] = []
+  ): ComponentRef<C> | undefined {
+    return this.insertComponent(
+      viewContainerRef.element.nativeElement,
+      componentClass,
+      viewContainerRef.injector,
+      providers
+    );
   }
 }
