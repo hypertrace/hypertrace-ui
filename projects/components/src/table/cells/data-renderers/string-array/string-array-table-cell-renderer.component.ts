685ebff5c3b6fee11999c2c958ad491ce32db5da
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2022-09-21 13:54:27 +0530

    fix: tests

--- projects/components/src/table/cells/data-renderers/string-array/string-array-table-cell-renderer.component.ts
+++ projects/components/src/table/cells/data-renderers/string-array/string-array-table-cell-renderer.component.ts
@@ -11,17 +11,18 @@ import { TableCellAlignmentType } from '../../types/table-cell-alignment-type';
   styleUrls: ['./string-array-table-cell-renderer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <ng-container *ngIf="this.value?.length > 0; else emptyValueTemplate">
-      <div class="string-array-cell" [htTooltip]="summaryTooltip">
+    <div class="string-array-cell" [htTooltip]="this.value?.length > 0 ? summaryTooltip : undefined">
+      <ng-container *ngIf="this.value?.length > 0; else emptyValueTemplate">
         <span class="first-item">{{ this.value[0] | htDisplayString }}</span>
         <ht-x-more [count]="(this.value | slice: 1).length" displayStyle="${XMoreDisplay.Gray}"></ht-x-more>
+      </ng-container>
 
-        <ng-template #summaryTooltip>
-          <div *ngFor="let value of this.value">{{ value }}</div>
-        </ng-template>
-      </div>
-    </ng-container>
-    <ng-template #emptyValueTemplate>-</ng-template>
+      <ng-template #summaryTooltip>
+        <div *ngFor="let value of this.value">{{ value }}</div>
+      </ng-template>
+
+      <ng-template #emptyValueTemplate>-</ng-template>
+    </div>
   `
 })
 @TableCellRenderer({
