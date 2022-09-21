bbf9c2ee94cf46a31ff8822a3fbc37d8cf0f8772
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2022-09-21 13:33:38 +0530

    fix: add empty value template for string array table clel renderer

--- projects/components/src/table/cells/data-renderers/string-array/string-array-table-cell-renderer.component.ts
+++ projects/components/src/table/cells/data-renderers/string-array/string-array-table-cell-renderer.component.ts
@@ -11,14 +11,17 @@ import { TableCellAlignmentType } from '../../types/table-cell-alignment-type';
   styleUrls: ['./string-array-table-cell-renderer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="string-array-cell" [htTooltip]="summaryTooltip">
-      <span class="first-item">{{ this.value[0] | htDisplayString }}</span>
-      <ht-x-more [count]="(this.value | slice: 1).length" displayStyle="${XMoreDisplay.Gray}"></ht-x-more>
+    <ng-container *ngIf="this.value?.length > 0; else emptyValueTemplate">
+      <div class="string-array-cell" [htTooltip]="summaryTooltip">
+        <span class="first-item">{{ this.value[0] | htDisplayString }}</span>
+        <ht-x-more [count]="(this.value | slice: 1).length" displayStyle="${XMoreDisplay.Gray}"></ht-x-more>
 
-      <ng-template #summaryTooltip>
-        <div *ngFor="let value of this.value">{{ value }}</div>
-      </ng-template>
-    </div>
+        <ng-template #summaryTooltip>
+          <div *ngFor="let value of this.value">{{ value }}</div>
+        </ng-template>
+      </div>
+    </ng-container>
+    <ng-template #emptyValueTemplate>-</ng-template>
   `
 })
 @TableCellRenderer({
