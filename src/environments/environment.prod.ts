1299a13fdbbac8fc32e1018902ba1f5e7e4c9931
Jay Bhatt <jay.bhatt@razorpay.com>
2022-09-27 17:47:19 +0530

    chore: add uri for deployments endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   graphqlUri: '/graphql',
   userPreferencesUri: '/user-preferences',
+  deploymentServiceUri: '/deployments',
   appTitle: 'Hypertrace'
 };
