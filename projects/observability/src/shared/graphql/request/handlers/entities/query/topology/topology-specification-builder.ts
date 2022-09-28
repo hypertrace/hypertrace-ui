46023afd972f8f48bd14a13ae0265769f5f6e1a0
Sandeep Kumar Sharma <sandeep.sharma@traceable.ai>
2022-09-28 21:53:20 +0530

    feat: add topology specification builder

---
+++ projects/observability/src/shared/graphql/request/handlers/entities/query/topology/topology-specification-builder.ts
@@ -0,0 +1,53 @@
+import { GraphQlSelection } from '@hypertrace/graphql-client';
+import { Specification } from '../../../../../model/schema/specifier/specification';
+import { GraphQlObservabilityArgumentBuilder } from '../../../../builders/argument/graphql-observability-argument-builder';
+import { GraphQlSelectionBuilder } from '../../../../builders/selections/graphql-selection-builder';
+
+export class TopologySpecificationBuilder {
+  private readonly argBuilder: GraphQlObservabilityArgumentBuilder = new GraphQlObservabilityArgumentBuilder();
+  private readonly selectionBuilder: GraphQlSelectionBuilder = new GraphQlSelectionBuilder();
+
+  public buildNeighborSpecification(config: NeighborSpecificationBuildConfig): Specification {
+    const alias = `${config.edgeDirection}_${config.neighborType}`;
+
+    return {
+      name: alias,
+      resultAlias: () => alias,
+      asGraphQlSelections: () => this.asGraphQlSelections(config, alias),
+      extractFromServerData: serverData => serverData[alias],
+      asGraphQlOrderByFragment: () => ({ expression: { key: '' } }) // Needed by the interface
+    };
+  }
+
+  private asGraphQlSelections(config: NeighborSpecificationBuildConfig, alias: string): GraphQlSelection[] {
+    return [
+      {
+        path: config.edgeDirection,
+        alias: alias,
+        arguments: [this.argBuilder.forNeighborType(config.neighborType)],
+        children: [
+          {
+            path: 'results',
+            children: [
+              {
+                path: 'neighbor',
+                children: [{ path: 'id' }, ...this.selectionBuilder.fromSpecifications(config.nodeSpecifications)]
+              }
+            ]
+          }
+        ]
+      }
+    ];
+  }
+}
+
+export interface NeighborSpecificationBuildConfig {
+  edgeDirection: TopologyEdgeDirection;
+  nodeSpecifications: Specification[];
+  neighborType: string;
+}
+
+export const enum TopologyEdgeDirection {
+  Incoming = 'incomingEdges',
+  Outgoing = 'outgoingEdges'
+}
