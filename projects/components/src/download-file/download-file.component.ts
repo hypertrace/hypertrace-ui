2d6472438d013949d08d15e48f26ed1d112437a2
Sandeep Sharma <78212931+itssharmasandeep@users.noreply.github.com>
2022-10-21 00:41:25 +0530

    feat: file download service (#1836)

--- projects/components/src/download-file/download-file.component.ts
+++ projects/components/src/download-file/download-file.component.ts
@@ -1,18 +1,16 @@
-import { DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, Input, Renderer2 } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { IconType } from '@hypertrace/assets-library';
-import { catchError, finalize, take } from 'rxjs/operators';
 import { ButtonSize, ButtonStyle } from '../button/button';
 import { IconSize } from '../icon/icon-size';
-import { NotificationService } from '../notification/notification.service';
 import { DownloadFileMetadata } from './download-file-metadata';
+import { FileDownloadService } from './service/file-download.service';
 
 @Component({
   selector: 'ht-download-file',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./download-file.component.scss'],
   template: `
-    <div *ngIf="this.metadata" class="download-file" (click)="this.triggerDownload()">
+    <div *ngIf="this.metadata" class="download-file" (click)="this.triggerDownload(this.metadata)">
       <ht-button
         *ngIf="!this.dataLoading"
         class="download-button"
@@ -29,39 +27,17 @@ export class DownloadFileComponent {
   public metadata?: DownloadFileMetadata;
 
   public dataLoading: boolean = false;
-  private readonly dlJsonAnchorElement: HTMLAnchorElement;
 
   public constructor(
-    @Inject(DOCUMENT) private readonly document: Document,
-    private readonly renderer: Renderer2,
-    private readonly changeDetector: ChangeDetectorRef,
-    private readonly notificationService: NotificationService
-  ) {
-    this.dlJsonAnchorElement = this.document.createElement('a');
-  }
+    private readonly cdr: ChangeDetectorRef,
+    private readonly fileDownloadService: FileDownloadService
+  ) {}
 
-  public triggerDownload(): void {
+  public triggerDownload(metadata: DownloadFileMetadata): void {
     this.dataLoading = true;
-    this.metadata!.dataSource.pipe(
-      take(1),
-      catchError(() => this.notificationService.createFailureToast('Download failed')),
-      finalize(() => {
-        this.dataLoading = false;
-        this.changeDetector.detectChanges();
-      })
-    ).subscribe((data: string) => {
-      this.downloadData(data);
+    this.fileDownloadService.downloadAsText(metadata).subscribe(() => {
+      this.dataLoading = false;
+      this.cdr.detectChanges();
     });
-  }
-
-  private downloadData(data: string): void {
-    this.renderer.setAttribute(
-      this.dlJsonAnchorElement,
-      'href',
-      `data:text/plain;charset=utf-8,${encodeURIComponent(data)}`
-    );
-    this.renderer.setAttribute(this.dlJsonAnchorElement, 'download', this.metadata!.fileName);
-    this.renderer.setAttribute(this.dlJsonAnchorElement, 'display', 'none');
-    this.dlJsonAnchorElement.click();
   }
 }
