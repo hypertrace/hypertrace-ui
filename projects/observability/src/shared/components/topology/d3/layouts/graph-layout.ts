8205306bf0d732634ef87cc3a6f913a412bcd4fb
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2022-10-22 01:17:43 +0530

    fix: recursive bfs, got rid of unnecessary map, readability improvements

--- projects/observability/src/shared/components/topology/d3/layouts/graph-layout.ts
+++ projects/observability/src/shared/components/topology/d3/layouts/graph-layout.ts
@@ -1,76 +1,72 @@
-import { RenderableTopology, RenderableTopologyNode, TopologyCoordinates, TopologyEdge, TopologyNode } from "../../topology";
+import { RenderableTopology, RenderableTopologyNode, TopologyEdge, TopologyNode } from "../../topology";
 
 export class GraphLayout {
 
-  private levelToNodesMap = new Map<number, RenderableTopologyNode[]>();
+  private levelToNodesMap = new Map<number, RenderableTopologyNode[]>([[0, []]]);
   private nodeToLevelMap = new Map<RenderableTopologyNode, number>();
-  private nodeCoOrdinatesMap: Map<TopologyNode, TopologyCoordinates> = new Map<TopologyNode, TopologyCoordinates>();
 
   public layout(topology: RenderableTopology<TopologyNode, TopologyEdge>): void {
 
     this.findRootNodes(topology);
     this.fillNodeAndLevelMaps();
-    this.fillNodeCoOrdinatesMap();
-    this.setNodeCoOrdinates(topology);
+    this.assignCoordinatesToNodes();
   }
 
   private findRootNodes(topology: RenderableTopology<TopologyNode, TopologyEdge>) {
     topology.nodes.forEach(node => {
       if (node.incoming.length === 0) {
         this.nodeToLevelMap.set(node, 0);
-        this.levelToNodesMap.has(0) ? this.levelToNodesMap.get(0)?.push(node) : this.levelToNodesMap.set(0, [node])
+        this.levelToNodesMap.get(0)?.push(node);
       }
     });
   }
 
   private fillNodeAndLevelMaps() {
 
-    const nodes = [...(this.levelToNodesMap.get(0) ?? [])];
-    const visited = new Set<TopologyNode>([...nodes.map(node => node.userNode)]);
+    const nodesThatAreGoingToBeExplored = [...(this.levelToNodesMap.get(0) ?? [])];
+    const nodesThatHaveBeenOrAreGoingToBeExplored = new Set<TopologyNode>([...nodesThatAreGoingToBeExplored.map(node => node.userNode)]);
 
-    while (nodes.length > 0) {
-      const parent = nodes[0];
-      const level = this.nodeToLevelMap.get(parent)! + 1;
-
-      parent.outgoing.forEach(edge => {
-        const child = edge.target;
-        if (!visited.has(child.userNode)) {
-          visited.add(child.userNode);
-          nodes.push(child);
-          this.nodeToLevelMap.set(child, level);
-          this.levelToNodesMap.has(level) ? this.levelToNodesMap.get(level)?.push(child) : this.levelToNodesMap.set(level, [child]);
-        }
-      });
-
-      nodes.shift();
-    }
+    this.levelOrderTraversal(nodesThatAreGoingToBeExplored, nodesThatHaveBeenOrAreGoingToBeExplored);
   }
 
-  private fillNodeCoOrdinatesMap() {
+  private levelOrderTraversal(nodes: RenderableTopologyNode[], visited: Set<TopologyNode>): void{
+    if(nodes.length === 0) {
+      return;
+    }
 
-    console.log(this.levelToNodesMap);
+    const parent = nodes[0];
+    nodes.shift();
+    const level = this.nodeToLevelMap.get(parent)! + 1;
+
+    parent.outgoing.forEach(edge => {
+      const child = edge.target;
+      if (!visited.has(child.userNode)) {
+        visited.add(child.userNode);
+        nodes.push(child);
+        this.nodeToLevelMap.set(child, level);
+        this.levelToNodesMap.has(level) ? this.levelToNodesMap.get(level)?.push(child) : this.levelToNodesMap.set(level, [child]);
+      }
+    });
+
+    this.levelOrderTraversal(nodes, visited);
+  }
+
+  private assignCoordinatesToNodes() {
 
     let curX = 1;
 
-    Array.from(this.levelToNodesMap.keys()).forEach(level => {
-      const nodes = this.levelToNodesMap.get(level)!;
+    Array.from(this.levelToNodesMap.values()).forEach(nodes => {
 
       let curY = 1;
       let maxWidth = 0;
       nodes.forEach((node) => {
-        this.nodeCoOrdinatesMap.set(node.userNode, { x: curX, y: curY });
+        node.x = curX;
+        node.y = curY;
         curY += (node.renderedData()?.getBoudingBox()?.height ?? 36) + 20;
         maxWidth = Math.max(maxWidth, node.renderedData()?.getBoudingBox()?.width ?? 400);
       })
 
       curX += maxWidth + 240;
-    });
-  }
-
-  private setNodeCoOrdinates(topology: RenderableTopology<TopologyNode, TopologyEdge>) {
-    topology.nodes.forEach(node => {
-      node.x = this.nodeCoOrdinatesMap.get(node.userNode)!.x;
-      node.y = this.nodeCoOrdinatesMap.get(node.userNode)!.y;
     });
   }
 }
