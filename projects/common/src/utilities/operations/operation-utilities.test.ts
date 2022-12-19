dbc9a19f7b1b9641f5de09e84cecba91d236acbc
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2022-12-19 14:01:49 +0530

    fix: failing tests in operation-utilities (#114)

--- projects/common/src/utilities/operations/operation-utilities.test.ts
+++ projects/common/src/utilities/operations/operation-utilities.test.ts
@@ -2,11 +2,15 @@ import { getDifferenceInDays } from './operation-utilities';
 
 describe('get difference in days', () => {
   it('should return no. of days from start date till current date', () => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date('2022-12-16').getTime());
     const result = getDifferenceInDays('1671069000000-1671159000000');
     expect(result).toBe(1);
   });
 
   it('should return 0 if current date and start date is on the same day', () => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date('2022-12-16').getTime());
     const result = getDifferenceInDays('1671155400000-1671159000000');
     expect(result).toBe(0);
   });
