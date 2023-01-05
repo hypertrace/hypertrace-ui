813b87a91d82382f445458d81762c5e015cf52dc
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2023-01-04 16:03:51 -0800

    refactor: setting better url pattern for page view

--- projects/common/src/telemetry/user-telemetry-impl.service.ts
+++ projects/common/src/telemetry/user-telemetry-impl.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Injector, Optional } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivationEnd, Router } from '@angular/router';
+import { isEmpty } from 'lodash-es';
 import { delay, filter } from 'rxjs/operators';
+import { HtRouteData } from '../navigation/ht-route-data';
 import { Dictionary } from '../utilities/types/types';
 import { UserTelemetryProvider, UserTelemetryRegistrationConfig, UserTraits } from './telemetry';
 import { UserTelemetryService } from './user-telemetry.service';
@@ -75,14 +77,18 @@ export class UserTelemetryImplService extends UserTelemetryService {
   private setupAutomaticPageTracking(): void {
     this.router?.events
       .pipe(
-        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+        filter((event): event is ActivationEnd => event instanceof ActivationEnd),
         delay(50)
       )
-      .subscribe(route => {
-        const absoluteUrl = route.url.split('?')?.[0];
-        const queryParams = this.router?.parseUrl(route.url).queryParams;
+      .subscribe(event => {
+        const url = event.snapshot.pathFromRoot
+          .map(snapshot => snapshot.routeConfig?.path)
+          .filter(path => !isEmpty(path))
+          .join('/');
+        const title = (event.snapshot.data as HtRouteData).title ?? url;
+        const queryParams = event.snapshot.queryParams;
 
-        this.trackPageEvent(`Visited: ${absoluteUrl}`, { url: route.url, absoluteUrl: absoluteUrl, ...queryParams });
+        this.trackPageEvent(`${url}`, { url: url, title: title, ...queryParams });
       });
   }
 }
