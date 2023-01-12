301dbc36ad142d3d3da2503567035037df5441c2
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2023-01-12 19:49:40 +0530

    fix: lint

--- projects/common/src/utilities/validators/form-validators.ts
+++ projects/common/src/utilities/validators/form-validators.ts
@@ -1,14 +1,13 @@
 import { AbstractControl } from '@angular/forms';
-import {getStringsFromCommaSeparatedList} from "../formatters/string/string-formatter";
-import {areEmailAddressesValid} from "./email-validator";
+import { getStringsFromCommaSeparatedList } from '../formatters/string/string-formatter';
+import { areEmailAddressesValid } from './email-validator';
 
 export const validateCommaSeparatedEmails = (control: AbstractControl) => {
   const emails = getStringsFromCommaSeparatedList(control.value);
-
   // tslint:disable: no-null-keyword
   return control.value.length === 0
     ? { emptyEmails: true }
     : !areEmailAddressesValid(emails)
-      ? { invalidEmails: true }
-      : null;
+    ? { invalidEmails: true }
+    : null;
 };
