27ab876d24c5f5c07858ea859320c4fd32d1c575
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2023-01-12 22:23:04 +0530

    fix: namespace all the custom validators

--- projects/common/src/utilities/validators/form-validators.ts
+++ projects/common/src/utilities/validators/form-validators.ts
@@ -2,13 +2,11 @@ import { AbstractControl } from '@angular/forms';
 import { getStringsFromCommaSeparatedList } from '../formatters/string/string-formatter';
 import { areEmailAddressesValid } from './email-validator';
 
-export const validateCommaSeparatedEmails = (control: AbstractControl) => {
-  const emails = getStringsFromCommaSeparatedList(control.value);
+export namespace CustomFormValidators {
+  export const commaSeparatedEmails = (control: AbstractControl) => {
+    const emails = getStringsFromCommaSeparatedList(control.value);
 
-  // tslint:disable: no-null-keyword
-  return control.value.length === 0
-    ? { emptyEmails: true }
-    : !areEmailAddressesValid(emails)
-    ? { invalidEmails: true }
-    : null;
-};
+    // tslint:disable: no-null-keyword
+    return !areEmailAddressesValid(emails) ? { invalidEmails: true } : null;
+  };
+}
