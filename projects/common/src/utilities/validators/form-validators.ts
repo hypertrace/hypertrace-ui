6a17b14122aafa7029dcf013f77d84b63dbcdcdc
Ankit Das <ankitdas@Ankits-MacBook-Pro.local>
2023-01-12 22:51:27 +0530

    fix: lint

--- projects/common/src/utilities/validators/form-validators.ts
+++ projects/common/src/utilities/validators/form-validators.ts
@@ -2,6 +2,7 @@ import { AbstractControl } from '@angular/forms';
 import { getStringsFromCommaSeparatedList } from '../formatters/string/string-formatter';
 import { areEmailAddressesValid } from './email-validator';
 
+// tslint:disable-next-line: no-namespace
 export namespace CustomFormValidators {
   export const commaSeparatedEmails = (control: AbstractControl) => {
     const emails = getStringsFromCommaSeparatedList(control.value);
