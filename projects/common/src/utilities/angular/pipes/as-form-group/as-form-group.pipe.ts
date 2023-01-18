6a69afc58d1ac859715c97aaca94ad4d7d145ff9
Thomas Mclennan <tgmclennan@hotmail.com>
2023-01-17 18:23:31 -0800

    fix: use correct prefix for pipe name

--- projects/common/src/utilities/angular/pipes/as-form-group/as-form-group.pipe.ts
+++ projects/common/src/utilities/angular/pipes/as-form-group/as-form-group.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
 
 @Pipe({
-  name: 'traceAsFormGroup'
+  name: 'htAsFormGroup'
 })
 export class AsFormGroupPipe implements PipeTransform {
   public transform(value: AbstractControl | undefined): FormGroup | undefined {
