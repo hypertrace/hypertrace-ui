3a8ec4f7bbbe5be02f0d46aa898c00a2837321ca
Jake Bassett <jake@traceable.ai>
2023-03-03 12:39:00 -0800

    feat: update commaSeparatedEmail form validator to handle isNil

--- projects/common/src/utilities/validators/form-validators.ts
+++ projects/common/src/utilities/validators/form-validators.ts
@@ -1,10 +1,15 @@
 import { AbstractControl } from '@angular/forms';
+import { isEmpty, isNil } from 'lodash-es';
 import { getStringsFromCommaSeparatedList } from '../formatters/string/string-formatter';
 import { areEmailAddressesValid } from './email-validator';
 
 // tslint:disable-next-line: no-namespace
 export namespace CustomFormValidators {
   export const commaSeparatedEmails = (control: AbstractControl) => {
+    if (isNil(control.value) || isEmpty(control.value)) {
+      return null;
+    }
+
     const emails = getStringsFromCommaSeparatedList(control.value);
 
     // tslint:disable: no-null-keyword
