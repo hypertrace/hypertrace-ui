a524bff1f95c0906c8941b9d8fc712349c55cab6
Thomas Mclennan <tgmclennan@hotmail.com>
2023-05-29 15:40:47 -0700

    feat: run change detection after updating the visible columns (#2227)
    * feat: run change detection after updating the visible columns
    
    - if we do not run change detection after updating the visible columns, then we run the risk of columns having a width of  (in such a case, the table renders visually as a tall 1px column)
    
    * test: ensure expanded column has a width greater than 0

--- projects/components/src/table/table.component.ts
+++ projects/components/src/table/table.component.ts
@@ -692,6 +692,7 @@ export class TableComponent
   private updateVisibleColumns(visibleColumnConfigs: TableColumnConfigExtended[]): void {
     this.visibleColumnConfigs = visibleColumnConfigs;
     this.visibleColumnIds = this.visibleColumnConfigs.map(column => column.id);
+    this.changeDetector.detectChanges();
   }
 
   private initializeData(): void {
