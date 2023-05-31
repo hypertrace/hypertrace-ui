4c77d69cc600c418b78391f5d944767ad2eb32f9
Patricio Albizu <patricioalbizu@Patricios-MacBook-Pro.local>
2023-05-31 20:44:28 +0200

    feat: Improving tests duration

--- projects/components/src/collapsible-sidebar/collapsible-sidebar.component.test.ts
+++ projects/components/src/collapsible-sidebar/collapsible-sidebar.component.test.ts
@@ -1,7 +1,7 @@
-import { fakeAsync } from '@angular/core/testing';
 import { IconType } from '@hypertrace/assets-library';
 import { createHostFactory } from '@ngneat/spectator/jest';
-import { MockComponent } from 'ng-mocks';
+import { MockComponents } from 'ng-mocks';
+import { EventBlockerComponent } from '../event-blocker/event-blocker.component';
 import { IconComponent } from '../icon/icon.component';
 import { CollapsibleSidebarComponent } from './collapsible-sidebar.component';
 
@@ -9,10 +9,10 @@ describe('Collapsible Sidebar Component', () => {
   const createHost = createHostFactory({
     component: CollapsibleSidebarComponent,
     shallow: true,
-    declarations: [MockComponent(IconComponent)]
+    declarations: [MockComponents(EventBlockerComponent, IconComponent)]
   });
 
-  test('should render content correclty', fakeAsync(() => {
+  test('should render content correclty', () => {
     const spectator = createHost(
       `<ht-collapsible-sidebar label="test-label"><div class="test-content"></div></ht-collapsible-sidebar>`
     );
@@ -22,10 +22,9 @@ describe('Collapsible Sidebar Component', () => {
     expect(spectator.query('.string-label')).toHaveText('test-label');
 
     spectator.click(spectator.query('.toggle') as Element);
-    spectator.tick();
 
     expect(spectator.query(IconComponent)?.icon).toBe(IconType.TriangleLeft);
     expect(spectator.query('.test-content')).toExist();
     expect(spectator.query('.string-label')).not.toExist();
-  }));
+  });
 });
