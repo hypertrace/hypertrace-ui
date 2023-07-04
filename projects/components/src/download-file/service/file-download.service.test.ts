89417e5f264d6d8665c0d4098cf1c81a21aedd4e
Arjunlal B <arjun@traceable.ai>
2023-07-04 19:31:11 +0530

    fix: update test

--- projects/components/src/download-file/service/file-download.service.test.ts
+++ projects/components/src/download-file/service/file-download.service.test.ts
@@ -64,18 +64,20 @@ describe('File Download Service', () => {
 
   test('should download as png correctly', () => {
     const spectator = createService();
-    jest.spyOn(window, 'fetch').mockReturnValue(
-      new Promise((resolve, reject) => {
-        resolve({
-          blob: () =>
-            new Promise((blobResolve, blobReject) => {
-              blobResolve(new Blob());
-              blobReject();
-            })
-        } as Response);
-        reject();
-      })
-    );
+    window.fetch = jest.fn().mockResolvedValue({
+      blob: jest.fn().mockResolvedValue(new Blob())
+    } as Partial<Response>);
+    // new Promise((resolve, reject) => {
+    //   resolve({
+    //     blob: () =>
+    //       new Promise((blobResolve, blobReject) => {
+    //         blobResolve(new Blob());
+    //         blobReject();
+    //       })
+    //   } as Response);
+    //   reject();
+    // })
+    // );
     // With correct data
     runFakeRxjs(({ expectObservable }) => {
       expectObservable(spectator.service.downloadPngFromUrl({ url: `image.png`, fileName: 'download.png' })).toBe(
