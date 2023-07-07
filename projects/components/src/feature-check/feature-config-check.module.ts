9cea89efb503520b4dc307b90ad5fcb778e15ef9
Arjunlal B <arjun@traceable.ai>
2023-07-07 12:03:51 +0530

    feat: isFeatureEnabled pipe

--- projects/components/src/feature-check/feature-config-check.module.ts
+++ projects/components/src/feature-check/feature-config-check.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FeaturePipe } from './feature.pipe';
 import { IfFeatureDirective } from './if-feature.directive';
+import { IsFeatureStateEnabled } from './is-feature-state-enabled.pipe';
 
 @NgModule({
-  declarations: [IfFeatureDirective, FeaturePipe],
-  exports: [IfFeatureDirective, FeaturePipe]
+  declarations: [IfFeatureDirective, FeaturePipe, IsFeatureStateEnabled],
+  exports: [IfFeatureDirective, FeaturePipe, IsFeatureStateEnabled]
 })
 export class FeatureConfigCheckModule {}
