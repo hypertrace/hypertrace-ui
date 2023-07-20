f8fb8bcbe971cbd09c9c5d96a61e6964c4b6c714
Sandeep Sharma <78212931+itssharmasandeep@users.noreply.github.com>
2023-07-20 16:32:45 +0530

    feat: tooltip support for toggle group item (#2315)

--- projects/components/src/toggle-group/toggle-item.ts
+++ projects/components/src/toggle-group/toggle-item.ts
@@ -1,7 +1,9 @@
+import { TemplateRef } from '@angular/core';
 import { IconType } from '@hypertrace/assets-library';
 
 export interface ToggleItem<TValue = unknown> {
   label?: string;
   icon?: IconType;
   value?: TValue;
+  tooltip?: string | TemplateRef<unknown>;
 }
