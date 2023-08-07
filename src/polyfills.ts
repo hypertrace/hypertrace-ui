4a3cac264b6682bd99b9fd1582d2340579cb4c78
Anand Tiwary <anand@traceable.ai>
2023-08-07 15:20:03 +0530

    refactor: fix lint errors

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,7 +56,6 @@
  * Zone JS is required by default for Angular itself.
  */
 
-/* eslint-disable import/no-unassigned-import */
 import 'zone.js'; // Included with Angular CLI.
 import 'zone.js/plugins/zone-patch-rxjs';
 /***************************************************************************************************
