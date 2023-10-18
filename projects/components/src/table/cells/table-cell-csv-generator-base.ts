8f3b340fd13750083a5cfad09e31285ec64d75c2
Arjunlal B <arjun@traceable.ai>
2023-10-18 20:12:27 +0530

    fix: update celltype

--- projects/components/src/table/cells/table-cell-csv-generator-base.ts
+++ projects/components/src/table/cells/table-cell-csv-generator-base.ts
@@ -1,12 +1,11 @@
 import { Dictionary } from '@hypertrace/common';
-import { CoreTableCellRendererType } from './types/core-table-cell-renderer-type';
 import { InjectionToken } from '@angular/core';
 
 export const TABLE_CELL_CSV_GENERATORS = new InjectionToken<unknown[][]>('TABLE_CELL_CSV_GENERATORS');
 
 export interface TableCellCsvGeneratorBase<TCellData, TRowData = unknown> {
   readonly type: CSV_GENERATOR_TYPE;
-  readonly cellType: CoreTableCellRendererType | string;
+  readonly cellType: string;
 
   generateSafeCsv(cellData?: TCellData, rowData?: TRowData): string | Dictionary<string> | undefined;
 }
